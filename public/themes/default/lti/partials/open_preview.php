1afb73bf77862d95d7bf25ceced4dd453107644e
Jonathan Warner <jaxbot@gmail.com>
2014-05-13 09:04:40 -0400

    Fix #79 LTI Success screen typo

--- public/themes/default/lti/partials/open_preview.php
+++ public/themes/default/lti/partials/open_preview.php
@@ -5,7 +5,7 @@
 <section>
 	<div class="container">
 		<p>
-			Your widget has been connected succesfully. Students will see your widget embedded here instead of this message.
+			Your widget has been connected successfully. Students will see your widget embedded here instead of this message.
 		</p>
 		<a target="_blank" class="button" href="<?= $preview_url ?>">Preview widget in new tab</a>
 	</div>
