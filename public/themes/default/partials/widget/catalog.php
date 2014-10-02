9ecf511cbf1cee3af10a0beba5f6ad62f0ccd5e3
Ivey <iveysaurrr@gmail.com>
2014-10-02 10:40:09 -0400

    Mostly angularized

--- public/themes/default/partials/widget/catalog.php
+++ public/themes/default/partials/widget/catalog.php
@@ -1,9 +1,38 @@
-<div class="container">
+<div ng-app="widgetApp" class="container">
 	<section class="page">
 		<div class="top">
 			<h1>Widget Catalog</h1>
 		</div>
-		<div class="widgets" data-container="catalog-wiget"></div>
+		<div class="widgets" ng-controller="widgetCtrl">
+			<section ng-repeat="widget in widgets" class="widget {{widget.clean_name}}" ng-mouseenter="showInfoCard(widget.id)">
+				<a href="/widgets/{{widget.id}}-{{widget.clean_name}}"><img src='{{widget.icon}}'></a>
+				<div class="header">
+					<h1><a href="/widgets/{{widget.id}}-{{widget.clean_name}}" class="searchable">{{widget.name}}</a></h1>
+				</div>
+				<dl class="left inline_def blurb">
+					<dt data-type="description">Description:</dt>
+					<dd>{{widget.meta_data['excerpt']}}</dd>
+				</dl>
+				<div ng-show="infoCard[widget.id]" class="infocard" style="width: 356px; display: block; margin-top: 5px; -webkit-transition: 1s ease-in-out;" ng-mouseleave="hideInfoCard()">
+					<a class="card-content" href="/widgets/{{widget.id}}-{{widget.clean_name}}" style="padding: 0">
+						<a href="/widgets/{{widget.id}}-{{widget.clean_name}}"><img src='{{widget.icon}}'></a>
+						<div class="header">
+							<h1 style="margin: 5px auto;"><a href="/widgets/{{widget.id}}-{{widget.clean_name}}" class="searchable" style="color: #fff; text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);">{{widget.name}}</a></h1>
+						</div>
+						<dl class="left inline_def blurb">
+							<dt data-type="description">Description:</dt>
+							<dd>{{widget.meta_data['excerpt']}}</dd>
+						</dl>
+						<dl class="left inline_def features_list" style="display: block">
+							<dt data-type="features">Features:</dt>
+							<dd ng-repeat="feature in widget.meta_data['features']" class="searchable">{{feature}}</dd>
+							<dt ng-show="widget.meta_data['supported_data']" data-type="supported">Supported Data:</dt>
+							<dd ng-repeat="supported in widget.meta_data['supported_data']" class="searchable">{{supported}}</dd>
+						</dl>
+					</a>
+				</div>
+			</section>
+		</div>
 	</section>
 
 	<aside>
@@ -25,22 +54,3 @@
 </div>
 
 <?= Theme::instance()->view('partials/notification') ?>
-
-<script type="text/template" id="t-widget-card"><section class="widget template" data-template="catalog-widget">
-	<a href="/widgets/detail/_template_clean_name"><img src='/assets/img/default/default-icon-92.png'></a>
-	<div class="header">
-		<h1><a href="/widgets/detail/_template_clean_name" class="searchable">_template_name</a></h1>
-	</div>
-	<dl class="left inline_def blurb">
-		<dt data-type="description">Description:</dt>
-		<dd>_template_description</dd>
-	</dl>
-	<dl class="left inline_def features_list">
-		<dt data-type="features">Features:</dt>
-		<dd class="searchable">_template_feature</dd>
-		<dt data-type="supported">Supported Data:</dt>
-		<dd class="searchable">_template_supported</dd>
-		<dt data-type="project">Project:</dt>
-		<dd class="searchable">_template_project</dd>
-	</dl>
-</section></script>
