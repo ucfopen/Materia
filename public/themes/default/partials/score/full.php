a402cd6b5ac9e3584b144d07df921a037e77edf5
Jonathan Warner <jaxbot@gmail.com>
2014-10-03 13:35:37 -0400

    Stashing some initial conversions

--- public/themes/default/partials/score/full.php
+++ public/themes/default/partials/score/full.php
@@ -1,51 +1,41 @@
-<article class="container">
-	<script id="score_header_template" type="text/template">
-			<h1><%= data.title %> Scores:</h1>
+<article class="container" ng-app="scorePage" ng-controller="scorePageController">
+	<h1>{{ widget.title }} Scores:</h1>
 
-			<nav class="previous-attempts">
-				<h1>Prev. Attempts</h1>
-				<ul>
-					<% for (var k=0; k < data.attempts.length; k++) { %>
-						<% var attempt_num = data.attempts.length - k; %>
-							<li><a href="#attempt-<%= attempt_num %>">Attempt <%= attempt_num %>: <span class="score"><%= data.attempts[k].roundedPercent %>%</span> <span class="date"><%= data.dates[k] %></span></a></li>
-					<% } %>
-				</ul>
-			</nav>
+	<nav class="previous-attempts">
+		<h1>Prev. Attempts</h1>
+		<ul>
+			<li ng-repeat="attempt in data.attempts" ng-init="data.attempts.length - $index">
+				<a href="#attempt-{{ attempt_num }}">Attempt {{ attempt_num }}: <span class="score">{{ attempt.roundedPercent }}%</span> <span class="date">{{ data.dates[$index] }}</span>
+			</a></li>
+		</ul>
+	</nav>
 
-			<nav class="play-again">
-				<h1><a id="play-again" class="action_button" href="<%= data.href %>"><%= data.play_again %></a></h1>
-			</nav>
-	</script>
+	<nav class="play-again">
+		<h1><a id="play-again" class="action_button" href="{{ data.href }}">{{ data.play_again }}</a></h1>
+	</nav>
 	<header class="header"></header>
 
-	<script id="score_overview_template" type="text/template">
-			<div id="overview-score">
-				<hgroup>
-					<h1>Attempt <span class="attempt-num"><%= data.attempt_num %></span> Score:</h1>
-					<h2 class="overall_score"><%= data.score %><span class="percent">%</span></h2>
-				</hgroup>
-				<div id="class-rank-button" class="action_button gray" >Compare With Class</div>
-			</div>
+	<div id="overview-score">
+		<hgroup>
+			<h1>Attempt <span class="attempt-num">{{ data.attempt_num }}</span> Score:</h1>
+			<h2 class="overall_score">{{ data.score }}<span class="percent">%</span></h2>
+		</hgroup>
+		<div id="class-rank-button" class="action_button gray" >Compare With Class</div>
+	</div>
 
-			<div id="overview-table">
-				<table>
-					<tbody>
-						<% for (var i=0; i < data.table.length; i++) { %>
-							<% var row = data.table[i]; %>
-								<tr>
-									<td><%= row.message %></td>
-									<% var sign = (row.value > -1) ? 'positive' : 'negative' %>
-									<td class="<%= sign %> number"><%= row.value %><%= (row.symbol == null) ? '%' : row.symbol %></td>
-								</tr>
-						<% } %>
-					</tbody>
-				</table>
-			</div>
-	</script>
+	<div id="overview-table">
+		<table>
+			<tbody>
+				<tr ng-repeat="row in data.table">
+					<td>{{ row.message }}</td>
+					<td class="{{ (row.value > -1) ? 'positive' : 'negative' }} number">
+						{{ row.value }}{{ (row.symbol == null) ? '%' : row.symbol }}
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 	<section class="overview"></section>
-
-	<script id="score_graph_template" type="text/template">
-	</script>
 
 	<section class="score-graph">
 		<div class="graph">
@@ -54,44 +44,31 @@
 		</div>
 	</section>
 
-	<script id="score_details_template" type="text/template">
-		<% for (var i = 0; i < data.length; i++) { %>
-			<% var detail = data[i]; %>
-			<h1><%= detail.title %></h1>
-			<ul>
-				<li class="details_header">
-					<% for (var j = 0; j < detail.header.length; j++) { %>
-						<h3><%= detail.header[j] %></h3>
-					<% } %>
+	<div ng-repeat="detail in details">
+		<h1><%= detail.title %></h1>
+		<ul>
+			<li class="details_header">
+				<h3 ng-repeat="header in detail.header">
+					{{ header }}
+				</h3>
+			</li>
+			<div ng-repeat="row in detail.table">
+				<li class="{{ row.style }}">
+					<div class="index" ng-if="row.graphic != 'none'">
+						<canvas class="question-number" id="question-{{ $parent.$index+1 }} %>-<%= $index+1 %>" >
+							<p>{{ $parent.$index+1 }}</p>
+						</canvas>
+						<span ng-if="{{ row.display_score }}">
+							{{ row.score }}{{ row.symbol }}
+						</span>
+					</div>
+					<div class="{{ row.data_style[$index] }}" ng-repeat="data in row.data">{{ data }}</div>
 				</li>
-				<% for (var j = 0; j< detail.table.length; j++) { %>
-					<% var row = detail.table[j]; %>
-					<li class="<%= row.style %>">
-						<% if (row.graphic != "none") { %>
-							<div class="index">
-								<canvas class="question-number" id="question-<%= i+1 %>-<%= j+1 %>" >
-									<p><%= j+1 %></p>
-								</canvas>
-									<span>
-										<% if (row.display_score) { %>
-											<%= row.score %><%= row.symbol %>
-										<% } %>
-									</span>
-							</div>
-						<% } %>
-						<% for (var k = 0; k < row.data.length; k++) { %>
-							<<%= row.tag != null ? row.tag : 'div' %> class="<%= row.data_style[k] %>"><%= row.data[k] %></div>
-						<% } %>
-					</li>
-					<% if (row.feedback != null) { %>
-						<li class="feedback single_column">
-							<p><%= row.feedback %></p>
-						</li>
-					<% } %>
-				<% } %>
-			</ul>
-		<% } %>
-	</script>
+				<li ng-if="row.feedback != null" class="feedback single_column">
+					<p>{{ row.feedback }}</p>
+				</li>
+		</ul>
+	</div>
 	<section class="details"></section>
 
 </article>
