9a7879ceb09510e1c5a624bc11b267754826fb11
Jonathan Warner <jaxbot@gmail.com>
2015-01-07 16:40:59 -0500

    Improve how widget details look
    Update #268 UI tweaks for life-without-casset widget details page

--- public/themes/default/partials/widget/detail.php
+++ public/themes/default/partials/widget/detail.php
@@ -20,15 +20,19 @@
 		<p class="thumbnail_explination">Click on a thumbnail to view a screenshot</p>
 		<dl id="metaData" class="left inline_def">
 			<dt ng-show='widget.features.length'>Features:</dt>
-			<dd ng-repeat='feature in widget.features'>
-				<a class="feature" ng-mouseover="feature.show = true" ng-mouseout="feature.show = false">{{ feature.text }}</a>
-				<div class="tooltip" style="display: {{ feature.show ? 'inline-block' : 'none' }}">{{ feature.description }}</div>
-			</dd>
+			<div class="aligner">
+				<dd ng-repeat='feature in widget.features'>
+					<a class="feature" ng-mouseover="feature.show = true" ng-mouseout="feature.show = false">{{ feature.text }}</a>
+					<div class="tooltip" style="display: {{ feature.show ? 'inline-block' : 'none' }}">{{ feature.description }}</div>
+				</dd>
+			</div>
 			<dt ng-show='widget.supported_data.length'>Supported Data:</dt>
-			<dd ng-repeat='data in widget.supported_data'>
-				<a class="supported_data" ng-mouseover="data.show=true" ng-mouseout="data.show = false">{{ data.text }}</a>
-				<div class="tooltip" style="display: {{ data.show ? 'inline-block' : 'none' }}">{{ data.description }}</div>
-			</dd>
+			<div class="aligner">
+				<dd ng-repeat='data in widget.supported_data'>
+					<a class="supported_data" ng-mouseover="data.show=true" ng-mouseout="data.show = false">{{ data.text }}</a>
+					<div class="tooltip" style="display: {{ data.show ? 'inline-block' : 'none' }}">{{ data.description }}</div>
+				</dd>
+			</div>
 		</dl>
 		<section class="right widget_right_selection">
 			<h4>Want to see it in action?</h4>
