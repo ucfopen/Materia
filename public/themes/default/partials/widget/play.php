e804550002243e525aac39c9c9d0b924ed507cac
Jonathan Warner <jaxbot@gmail.com>
2015-01-16 10:41:28 -0500

    Fix the flash player warning on player

--- public/themes/default/partials/widget/play.php
+++ public/themes/default/partials/widget/play.php
@@ -1,9 +1,10 @@
 <section class="widget" ng-controller="playerCtrl" ng-init="inst_id = '<?= $inst_id ?>'">
 	<header ng-if="isPreview" ng-class="preview-bar"></header>
-	<div class="center">
+	<div class="center" ng-show="type == 'flash' || type == 'html'">
 		<iframe src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html"></iframe>
-		<div id="container" ng-if="type =='noflash'">
-			<?= Theme::instance()->view('partials/noflash') ?>
-		</div>
+		<div id="container" ng-if="type =='flash'"></div>
+	</div>
+	<div id="container" ng-if="type =='noflash'">
+		<?= Theme::instance()->view('partials/noflash') ?>
 	</div>
 </section>
