572c4b663c4e52ebaada3d3ea94143b01ab9d464
Ivey Padgett <iveysaurrr@gmail.com>
2015-02-03 17:31:01 -0500

    Remove acl and group classes from materia auth

--- fuel/packages/materiaauth/bootstrap.php
+++ fuel/packages/materiaauth/bootstrap.php
@@ -5,7 +5,5 @@ Autoloader::add_classes([
 	'Sso'                        => __DIR__.'/classes/sso.php',
 	'Auth_Login_Materiaauth'         => __DIR__.'/classes/auth/login/materiaauth.php',
 	'MateriaAuthUserUpdateException' => __DIR__.'/classes/auth/login/materiaauth.php',
-	'Auth_Group_Materiagroup'        => __DIR__.'/classes/auth/group/materiagroup.php',
-	'Auth_Acl_Materiaacl'            => __DIR__.'/classes/auth/acl/materiaacl.php',
 ]);
 \Event::register('lti_get_or_create_user', 'Auth_Login_Materiaauth::get_synced_user');
