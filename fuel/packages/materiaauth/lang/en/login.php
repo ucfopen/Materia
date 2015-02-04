cdf390e781ddf2ad7d88eeaa46e0ce2d777f6614
Ivey Padgett <iveysaurrr@gmail.com>
2015-02-04 13:04:00 -0500

    Add login file to materia auth

---
+++ fuel/packages/materiaauth/lang/en/login.php
@@ -0,0 +1,9 @@
+<?
+return [
+	'links' => [
+		'a' => ['href' => 'https://my.ucf.edu/nid.html', 'title' => 'Lookup NID'],
+		'b' => ['href' => 'http://mynid.ucf.edu/', 'title' => 'Reset Password'],
+	],
+	'user' => 'UCF NID',
+	'password' => 'NID Password',
+];
