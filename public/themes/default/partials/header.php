18f5b60686bb82934a5c14af5ed8a4e15bdcb699
Zachary Berry <zachberry@gmail.com>
2015-02-12 15:58:15 -0500

    Fixes some html bugs that IE10 complained about, fixes a spelling error on widget catalog page

--- public/themes/default/partials/header.php
+++ public/themes/default/partials/header.php
@@ -2,7 +2,7 @@
 
 	<? /* @TODO: this should maybe be retrieved via the api instead of mucking with the html here */ ?>
 	<? if (empty($me)): ?>
-		<span id="current-user" data-logged-in="false" />
+		<span id="current-user" data-logged-in="false"></span>
 	<? else: ?>
 		<span id="current-user"
 			data-logged-in="true"
@@ -10,7 +10,7 @@
 			data-avatar="<?= \Materia\Utils::get_avatar() ?>"
 			data-role="<?= \RocketDuck\Perm_Manager::does_user_have_role([\RocketDuck\Perm_Role::AUTHOR]) ? 'Staff' : 'Student' ?>"
 			data-notify="<?= $me->profile_fields['notify'] ? 'true' : 'false' ?>"
-		/>
+		></span>
 	<? endif ?>
 
 	<h1 class="logo"><a href="/">Materia</a></h1>
@@ -42,7 +42,7 @@
 		<div id="notices" ng-if="values.notifications.length > 0">
 			<div class="notice" ng-repeat="notification in values.notifications">
 				<a href="#" class="noticeClose" ng-click="removeNotification($index)"></a>
-				<p class="icon"><img class="senderAvatar" ng-src="{{notification.avatar}}"></img></p>
+				<p class="icon"><img class="senderAvatar" ng-src="{{notification.avatar}}"></p>
 				<div class="notice_right_side">
 					<p class="subject" ng-bind-html="trust(notification.subject)"></p>
 				</div>
