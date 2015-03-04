84933215803a70af01956ca54c29396b9bcc266e
Eric Colon <me@ericcolon.com>
2015-03-04 17:33:59 -0500

    updated export icon to something proper

--- public/themes/default/partials/my_widgets.php
+++ public/themes/default/partials/my_widgets.php
@@ -284,7 +284,7 @@
 				<div class="scores hidden-xs" ng-show="selected.widget.widget.is_scorable">
 					<h2>Student Activity</h2>
 					<span id="export_scores_button" class="action-button" ng-disabled="selected.scores.list.length == 0 || !selected.hasScores" ng-class="{'disabled': selected.scores.list.length == 0}" ng-click="exportPopup()">
-						<span class="fa fa-cog"></span>
+						<span class="fa fa-cloud-download"></span>
 						Export Scores
 					</span>
 
