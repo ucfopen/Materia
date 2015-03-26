f98636f8c61248ddd9af233cf92d166177555a6f
Corey Peterson <corey.peterson@ucf.edu>
2015-03-26 16:34:00 -0400

    Added SQL query output script

--- fuel/packages/materia/tasks/shoehorn.php
+++ fuel/packages/materia/tasks/shoehorn.php
@@ -4,6 +4,76 @@ namespace Fuel\Tasks;
 
 class Shoehorn extends \Basetask
 {
+	public static function output_ucf_id_conversion($to_file = false)
+	{
+		$materia_user_table = "users";
+		$cerebro_user_table = "CDLPS_PEOPLE";
+		$nid = "network_id";
+		$ucf_id = "pps_number";
+
+		\Cli::write("This will output the raw SQL for the conversion script.");
+		\Cli::write("Before continuing, make sure the database settings are correct");
+
+		$all_users = \DB::select("id", "username")
+			->from($materia_user_table)
+			->as_assoc();
+
+		if ($to_file) {
+			static::write_to_file($all_users->compile());
+		}
+		else
+		{
+			echo($all_users->compile()."\n");
+		}
+
+		$all_users = $all_users->execute();
+
+		foreach ($all_users as $user) {
+			if (preg_match("/^~{0}[a-zA-Z0-9]+$/", $user["username"]) == 0) {
+				continue;
+			}
+			else
+			{
+				$user_id = $user["id"];
+				$user_name = $user["username"];
+
+				$results = \DB::select("pps_number")
+					->from($cerebro_user_table)
+					->where($nid, $user_name)
+					->as_assoc();
+
+				if ($to_file) {
+					static::write_to_file($results->compile());
+				}
+				else
+				{
+					echo($results->compile()."\n");
+				}
+
+				$results = $results->execute("ucf");
+
+				if (count($results) > 0) {
+
+					$results_array = $results->as_array()[0];
+					$user_ucf_id = $results_array[$ucf_id];
+
+					$status = \DB::update("users")
+						->set([
+							'username' => $user_ucf_id
+						])
+						->where("id", $user_id);
+
+					if ($to_file) {
+						static::write_to_file($status->compile());
+					}
+					else
+					{
+						echo($status->compile()."\n");
+					}
+				}
+			}
+		}
+	}
 
 	public static function run_ucf_id_conversion()
 	{
@@ -75,7 +145,7 @@ class Shoehorn extends \Basetask
 			->from($cerebro_user_table)
 			->where($nid, $user_id)
 			->as_assoc()
-			->execute('ucf');
+			->execute("ucf");
 
 		if (count($results) > 0) {
 
@@ -111,6 +181,10 @@ class Shoehorn extends \Basetask
 			->execute();
 
 		return $status;
+	}
+
+	public static function write_to_file($str) {
+		file_put_contents("conversion_output.txt", $str."\n", FILE_APPEND);
 	}
 
 }
