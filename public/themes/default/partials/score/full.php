13b4a9ba0d8ec7be91833714198ce5d39fbad033
Jonathan Warner <jaxbot@gmail.com>
2015-05-13 13:50:23 -0400

    Update score page text

--- public/themes/default/partials/score/full.php
+++ public/themes/default/partials/score/full.php
@@ -83,11 +83,11 @@
 
 	<div class="score_restrict container general" ng-show="restricted">
 		<section class="page score_restrict">
-			<h2 class="logo">You don't have any scores for this widget.</h2>
+			<h2 class="logo">You don't have permission to view this page.</h2>
 
 			<p>You may need to:</p>
 			<ul>
-				<li>Make sure the score you're trying to access belongs to you.</li>
+				<li>Make sure the score you're trying to access belongs to you or your student.</li>
 				<li>Try to access this score through your profile page.</li>
 				<li>Check out our documentation.</li>
 			</ul>
