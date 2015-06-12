ed42739dea661e413a17155d75a2ba64152e2806
William Funk <wfunk@cdl215172.local>
2015-06-12 15:46:05 -0400

    Made changes to front end of student access for 'Edit Settings' to prevent them from toggling 'Guest Access'

--- fuel/app/classes/controller/widgets.php
+++ fuel/app/classes/controller/widgets.php
@@ -194,6 +194,8 @@ class Controller_Widgets extends Controller
 		// TODO: remove ngmodal, jquery, convert author to something else, materia is a mess
 		Js::push_group(['angular', 'ng_modal', 'jquery', 'materia', 'author', 'tablock', 'spinner', 'jqplot', 'my_widgets', 'dataTables']);
 
+		Js::push_inline('var STUDENT_LOGGED = '.(Materia\Api::session_valid('basic_author') ? 'false' : 'true'));
+
 		$this->theme->get_template()
 			->set('title', 'My Widgets')
 			->set('page_type', 'my_widgets');
