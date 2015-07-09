4835908dffc73cb852fd5a4c740d635ea2543803
iturgeon <iturgeon@gmail.com>
2015-07-09 14:50:46 -0400

    fixes to properly create exporter metadata and clean up installer a little bit

--- fuel/packages/materia/classes/widget/installer.php
+++ fuel/packages/materia/classes/widget/installer.php
@@ -145,26 +145,26 @@ class Widget_Installer
 			->execute();
 	}
 
-	public static function add_manifest($id, $manifest_data)
+	public static function save_metadata($id, $metadata)
 	{
 		// add in the metadata
-		foreach ($manifest_data['meta_data'] as $metadata_key => $metadata_value)
+		foreach ($metadata as $metadata_key => $metadata_value)
 		{
 			if (is_array($metadata_value))
 			{
 				foreach ($metadata_value as $metadata_child_item)
 				{
-					self::insert_metadata($id, $metadata_key, $metadata_child_item);
+					self::db_insert_metadata($id, $metadata_key, $metadata_child_item);
 				}
 			}
 			else
 			{
-				self::insert_metadata($id, $metadata_key, $metadata_value);
+				self::db_insert_metadata($id, $metadata_key, $metadata_value);
 			}
 		}
 	}
 
-	private static function insert_metadata($id, $key, $value)
+	private static function db_insert_metadata($id, $key, $value)
 	{
 		\DB::insert('widget_metadata')
 			->set([
@@ -383,20 +383,6 @@ class Widget_Installer
 		if (self::missing_required_attributes($score, ['is_scorable', 'score_module'])) return;
 		if (self::values_are_not_boolean($score, ['is_scorable'])) return;
 
-		// 6. make sure export section is correct by ensuring export methods listed in yaml match those in export module
-
-		// if the export module exists, include it and get its defined methods
-		// otherwise, don't fail validation, since export modules are not mandatory
-		$playdata_exporters_file = "{$dir}/_exports/playdata_exporters.php";
-		if (file_exists($playdata_exporters_file))
-		{
-			$methods = \Materia\Utils::load_methods_from_file($playdata_exporters_file);
-			if ( ! empty($methods))
-			{
-				$manifest_data['meta_data']['playdata_exporters'] = array_keys($methods);
-			}
-		}
-
 		// 7. make sure metadata section is correct
 		$metadata = $manifest_data['meta_data'];
 		if (self::missing_required_attributes($metadata, ['about', 'excerpt'])) return;
@@ -467,6 +453,10 @@ class Widget_Installer
 		$widget_dir = "{$id}-{$clean_name}";
 		$score_module_clean_name = strtolower(\Inflector::friendly_title($manifest_data['score']['score_module'])).'.php';
 
+		// create the widget specific directory
+		self::clear_path(PKGPATH.'materia/vendor/widget/'. $widget_dir);
+		$file_area->create_dir(PKGPATH.'materia/vendor/widget/', $widget_dir);
+
 		// score modules
 		$destination_score_module_file = PKGPATH.'materia/vendor/widget/score_module/'.$score_module_clean_name;
 		if (file_exists($destination_score_module_file)) $file_area->delete($destination_score_module_file);
@@ -480,15 +470,26 @@ class Widget_Installer
 		if (file_exists($pkg_playdata_file))
 		{
 			$destination_playdata_file = PKGPATH.'materia/vendor/widget/'.$widget_dir.'/playdata_exporters.php';
-			if (file_exists($destination_playdata_file)) $file_area->delete($destination_playdata_file);
+			static::clear_path($destination_playdata_file);
 			$file_area->rename($pkg_playdata_file, $destination_playdata_file);
 			// delete the export modules folder so it won't get copied over
 			$file_area->delete_dir($dir.'/_exports');
+
+			// add export methods to metadata
+			if (file_exists($destination_playdata_file))
+			{
+				$methods = \Materia\Utils::load_methods_from_file($destination_playdata_file);
+				if ( ! empty($methods))
+				{
+					$metadata = ['playdata_exporters' => array_keys($methods)];
+					self::save_metadata($id, $metadata);
+				}
+			}
 		}
 
 		// move tests
 		$new_test = PKGPATH.'materia/vendor/widget/test/'.$score_module_clean_name;
-		if (file_exists($new_test)) $file_area->delete($new_test);
+		static::clear_path($new_test);
 		$file_area->rename($dir.'/_score-modules/test_score_module.php', $new_test);
 
 		// move spec to the main materia spec folder, if it exists
@@ -496,7 +497,7 @@ class Widget_Installer
 		if (file_exists($pkg_spec))
 		{
 			$new_spec = APPPATH."../../spec/widgets/{$clean_name}.spec.coffee";
-			if (file_exists($new_spec)) $file_area->delete($new_spec);
+			static::clear_path($new_spec);
 			$file_area->rename($pkg_spec, $new_spec);
 		}
 
@@ -508,6 +509,13 @@ class Widget_Installer
 		$new_dir = \Config::get('materia.dirs.engines').$widget_dir;
 		if (is_dir($new_dir)) $file_area->delete_dir($new_dir);
 		$file_area->copy_dir($dir, $new_dir);
+	}
+
+	private static function clear_path($file)
+	{
+		$file_area = \File::forge(['basedir' => null]);
+		if (is_dir($file)) $file_area->delete_dir($file);
+		if (file_exists($file)) $file_area->delete($file);
 	}
 
 	public static function force_install($widget_file)
@@ -570,7 +578,7 @@ class Widget_Installer
 				return false;
 			}
 
-			self::add_manifest($id, $manifest_data);
+			self::save_metadata($id, $manifest_data['meta_data']);
 			self::install_widget_files($id, $manifest_data, $dir);
 			self::cleanup($dir);
 
