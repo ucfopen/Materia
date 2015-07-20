ee35a1135772ed7cca34e0162afbee722c0d06e8
David Perlaza <superlaza7@gmail.com>
2015-07-20 13:30:53 -0400

    wrapped iframe with div styled with overflow:scroll; scrolling issue fixed in play, hidden header in preview fixed; works in lti and iOS simulator

--- public/themes/default/partials/widget/play.php
+++ public/themes/default/partials/widget/play.php
@@ -1,8 +1,10 @@
 <section class="widget" ng-controller="playerCtrl" ng-init="inst_id = '<?= $inst_id ?>'" ng-class="{ preview: isPreview }">
 	<div class="center" ng-show="type == 'flash' || type == 'html'">
 		<header ng-if="isPreview" class="preview-bar"></header>
-		<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="no"></iframe>
-		<div id="container" ng-if="type =='flash'"></div>
+		<div class="widgetMainContent">
+			<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="yes"></iframe>
+			<div id="container" ng-if="type =='flash'"></div>
+		</div>
 	</div>
 	<div id="container" ng-if="type =='noflash'">
 		<?= Theme::instance()->view('partials/noflash') ?>
