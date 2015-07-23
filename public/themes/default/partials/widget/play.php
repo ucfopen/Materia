31a5a79cb0ab7d01b21b3449365a77b132fe7c1c
David Perlaza <superlaza7@gmail.com>
2015-07-23 13:42:36 -0400

    undo height/overflow style changes that need to be matured before merging

--- public/themes/default/partials/widget/play.php
+++ public/themes/default/partials/widget/play.php
@@ -1,7 +1,7 @@
 <section class="widget" ng-controller="playerCtrl" ng-init="inst_id = '<?= $inst_id ?>'" ng-class="{ preview: isPreview }">
 	<div class="center" ng-show="type == 'flash' || type == 'html'">
 		<header ng-if="isPreview" class="preview-bar"></header>
-		<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="yes"></iframe>
+		<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="no"></iframe>
 		<div id="container" ng-if="type =='flash'"></div>
 	</div>
 	<div id="container" ng-if="type =='noflash'">
