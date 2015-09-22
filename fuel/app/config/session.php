af83f4099d2fb3ade6a7c78aacd0f88053d46864
Ivey Padgett <iveysaurrr@gmail.com>
2015-09-22 11:27:34 -0400

    Change session storage to memcached

--- fuel/app/config/session.php
+++ fuel/app/config/session.php
@@ -1,12 +1,17 @@
 <?php
 
 return array(
-	'driver' => 'file',
-	// specific configuration settings for file based sessions
-	'file'				=> array(
-		'cookie_name'		=> 'fuelfid',				// name of the session cookie for file based sessions
-		'path'				=>	APPPATH.'/tmp',					// path where the session files should be stored
-		'gc_probability'	=>	5						// probability % (between 0 and 100) for garbage collection
+	'driver' => 'memcached',
+	// specific configuration settings for memcached based sessions
+	'memcached' => array(
+		'cookie_name' => 'fuelmid', // name of the session cookie for memcached based sessions
+		'servers' => array( // contains a list of available memcached servers
+			'default' => array(
+				'host'   => '127.0.0.1',
+				'port'   => 11211,
+				'weight' => 100
+			)
+		)
 	),
 	'expiration_time'	=> 21600,
 );
