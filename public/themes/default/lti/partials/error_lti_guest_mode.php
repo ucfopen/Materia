1a0ab304aaf7b24e9ddc3ff19992ca568b3c4611
Jonathan Warner <jaxbot@gmail.com>
2015-10-06 15:42:46 -0400

    Update the placeholder text

--- public/themes/default/lti/partials/error_lti_guest_mode.php
+++ public/themes/default/lti/partials/error_lti_guest_mode.php
@@ -5,7 +5,8 @@
 
 <section id="error-container">
 	<p>This assignment has guest mode enabled.</p>
-	<p>Your instructor will need to git gud.</p>
+	<p>This assignment can only record scores anonymously and therefore cannot be played as an embedded assignment.</p>
+	<p>Your instructor will need to disable guest mode or provide a link to play as a guest.</p>
 
 	<?= Theme::instance()->view('partials/help/support_info') ?>
 </section>
