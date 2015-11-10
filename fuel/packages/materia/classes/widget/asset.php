07f724dc726e3422fd24d419e86f91d21ab4a5cd
William Funk <contact@WilliamRADFunk.com>
2015-11-10 10:53:08 -0500

    Adjusting asset object to take in additional variable.

--- fuel/packages/materia/classes/widget/asset.php
+++ fuel/packages/materia/classes/widget/asset.php
@@ -34,6 +34,7 @@ class Widget_Asset
 	public $id         = 0;
 	public $is_shared;
 	public $title      = '';
+	public $remote_url = '';
 	public $file_size  = '';
 	public $questions  = [];
 	public $type       = '';
@@ -80,6 +81,7 @@ class Widget_Asset
 						'id'          => $hash,
 						'type'        => $this->type,
 						'title'       => $this->title,
+						'remote_url'  => $this->remote_url
 						'file_size'   => $this->file_size,
 						'created_at'  => time()
 					])
