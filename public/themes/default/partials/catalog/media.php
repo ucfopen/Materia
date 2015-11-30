8213219e9312cd9b4fdf52d1aef32621117cbb88
William Funk <contact@WilliamRADFunk.com>
2015-11-30 12:11:58 -0500

    Changed placeholder text.

--- public/themes/default/partials/catalog/media.php
+++ public/themes/default/partials/catalog/media.php
@@ -12,9 +12,9 @@
 		</div>
 		<form id="embed-link-form" ng-show="videoImport">
 			<label class="main-video-labels">Title: </label><label id="title-error" data-ng-show="invalidTitle"> *You must enter a title for this upload.</label>
-			<input id="embed-link-title" type="text" data-ng-model="title" placeholder="Electro Swing Collection">
+			<input id="embed-link-title" type="text" data-ng-model="title" placeholder="Title of your video">
 			<label class="main-video-labels">Embed URL: </label><label id="link-error" data-ng-show="invalidLink"> *Not a valid YouTube embed link.</label>
-			<input id="embed-link" type="text" data-ng-model="link" placeholder="https://www.youtube.com/embed/JxKR7BaitxM">
+			<input id="embed-link" type="text" data-ng-model="link" placeholder="https://www.youtube.com/embed/">
 			<button id="btn-upload-video" type="submit" value="Upload" data-ng-click="submitVideoLink(title, link)">Upload</button>
 		</form>
 		<div class="pane-header" ng-show="imageAndAudioImport">
