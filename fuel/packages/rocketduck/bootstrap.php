881cb5fafc298a3dc75ab35b05d3bf5d20937928
iturgeon <iturgeon@gmail.com>
2016-02-15 11:08:48 -0500

    adds an abstraction method to simplify the annoying try/catch beeded for cache::get

--- fuel/packages/rocketduck/bootstrap.php
+++ fuel/packages/rocketduck/bootstrap.php
@@ -3,6 +3,7 @@ Autoloader::add_namespace('RocketDuck', PKGPATH.'rocketDuck/classes/');
 
 Autoloader::add_classes([
 	'File'                              => __DIR__.'/classes/fuel/core/file.php',
+	'Cache'                             => __DIR__.'/classes/fuel/core/cache.php',
 	'RocketDuck\\Db_Role'               => __DIR__.'/classes/db/role.php',
 	'RocketDuck\\Log'                   => __DIR__.'/classes/log.php',
 	'RocketDuck\\Perm_Acl'              => __DIR__.'/classes/perm/acl.php',
