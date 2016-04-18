c053a3c592b09b04004b75e22978c84d19800a05
Brandon Stull <bstull@ucf.edu>
2016-04-18 11:28:09 -0400

    #787 Modified LTI config XML and made new endpoint for an LTI login.

--- fuel/app/modules/lti/classes/controller/lti.php
+++ fuel/app/modules/lti/classes/controller/lti.php
@@ -25,11 +25,33 @@ class Controller_Lti extends \Controller
 			->set('title', \Config::get('lti::lti.consumers.canvas.title'))
 			->set('description', \Config::get('lti::lti.consumers.canvas.description'))
 			->set('launch_url', \Uri::create('lti/assignment'))
+			->set('login_url', \Uri::create('lti/login'))
 			->set('picker_url', \Uri::create('lti/picker'))
 			->set('platform', \Config::get('lti::lti.consumers.canvas.platform'))
 			->set('privacy_level', \Config::get('lti::lti.consumers.canvas.privacy'));
 
 		return \Response::forge($this->theme->render())->set_header('Content-Type', 'application/xml');
+	}
+
+	/**
+	 * LTI for logging into Materia through Canvas
+	 *
+	 */
+	public function action_login()
+	{
+		$launch = LtiLaunch::from_request();
+		if ( ! LtiUserManager::authenticate($launch)) \Response::redirect('/lti/error?message=invalid_oauth_request');
+
+		$this->theme->set_template('layouts/main')
+			->set('title', 'Materia')
+			->set('page_type', 'lti-login');
+
+		$this->theme->set_partial('content', 'partials/post_login');
+		$this->insert_analytics();
+
+		\Css::push_group('core');
+
+		return \Response::forge($this->theme->render());
 	}
 
 	/**
@@ -47,7 +69,7 @@ class Controller_Lti extends \Controller
 		$is_selector_mode = \Input::post('selection_directive') == 'select_link';
 		$return_url       = \Input::post('launch_presentation_return_url');
 
-		\RocketDuck\Log::profile(['action_picker', \Input::post('selection_directive'), $system, $is_selector_mode ? 'yes':'no', $return_url], 'lti');
+		\RocketDuck\Log::profile(['action_picker', \Input::post('selection_directive'), $system, $is_selector_mode ? 'yes' : 'no', $return_url], 'lti');
 
 		$this->theme->set_template('layouts/main');
 
---
+++ public/themes/default/lti/partials/post_login.php
@@ -0,0 +1,30 @@
+<section class='p_s'>
+	<div>
+		<h1>
+			You're logged into Materia!
+		</h1>
+	</div>
+	<div>
+		<h3>What Materia is.</h3>
+		<p>
+			Materia is a learning platform designed to enrich online learning with the help of custom-built games and tools.
+		</p>
+	</div>
+
+	<div>
+		<h3>How to make new widgets.</h3>
+		<p>
+			Materia features a growing library of customizable widgets.
+			Learn more about the available widgets and how to make your own <a target='_blank' href='http://ucfcdl.github.io/Materia/create/getting-started.html'>here</a>.
+		</p>
+	</div>
+
+	<div>
+		<h3>How to embed widgets.</h3>
+		<p>
+			Embedding the widgets you create into your Canvas courses as assignments - graded or not - is a quick and easy process.
+			Learn more about embedding your widgets <a target='_blank' href='http://ucfcdl.github.io/Materia/create/embedding-in-canvas.html'>here</a>.
+		</p>
+	</div>
+	<a class="action_button" target='_blank' href="/my-widgets">Go to Materia</a>
+</section>
