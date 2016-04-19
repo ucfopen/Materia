a641fde1bc1e3846697a2886e83a1bc91e40a175
Brandon Stull <bstull@ucf.edu>
2016-04-19 09:59:15 -0400

    #787 Functionally good, appearance and copy need some work.

--- public/themes/default/lti/partials/post_login.php
+++ public/themes/default/lti/partials/post_login.php
@@ -5,25 +5,25 @@
 		</h1>
 	</div>
 	<div>
-		<h3>What Materia is.</h3>
+		<h3>Materia:</h3>
 		<p>
 			Materia is a learning platform designed to enrich online learning with the help of custom-built games and tools.
 		</p>
 	</div>
 
 	<div>
-		<h3>How to make new widgets.</h3>
+		<h3>Make Your Own Widgets:</h3>
 		<p>
 			Materia features a growing library of customizable widgets.
-			Learn more about the available widgets and how to make your own <a target='_blank' href='http://ucfcdl.github.io/Materia/create/getting-started.html'>here</a>.
+			Learn more about the available widgets and how to make your own <a class='external' target='_blank' href='http://ucfcdl.github.io/Materia/create/getting-started.html'>here</a>.
 		</p>
 	</div>
 
 	<div>
-		<h3>How to embed widgets.</h3>
+		<h3>Embed Your Widgets:</h3>
 		<p>
 			Embedding the widgets you create into your Canvas courses as assignments - graded or not - is a quick and easy process.
-			Learn more about embedding your widgets <a target='_blank' href='http://ucfcdl.github.io/Materia/create/embedding-in-canvas.html'>here</a>.
+			Learn more about embedding your widgets <a class='external' target='_blank' href='http://ucfcdl.github.io/Materia/create/embedding-in-canvas.html'>here</a>.
 		</p>
 	</div>
 	<a class="action_button" target='_blank' href="/my-widgets">Go to Materia</a>
