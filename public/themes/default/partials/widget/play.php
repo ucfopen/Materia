198d4c9a50c35e0fc5bbf8f3aed84ccc35a65a77
Corey Peterson <corey.peterson@ucf.edu>
2017-04-25 14:58:36 -0400

    Old method of applying attribute didnt work, reworked as a directive

--- public/themes/default/partials/widget/play.php
+++ public/themes/default/partials/widget/play.php
@@ -1,7 +1,7 @@
 <section class="widget" ng-controller="playerCtrl" ng-init="inst_id = '<?= $inst_id ?>'" ng-class="{ preview: isPreview }">
 	<header ng-if="isPreview" class="preview-bar"></header>
 	<div class="center" ng-show="type == 'flash' || type == 'html'">
-		<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="yes" allowfullscreen="{{ allowFullScreen }}"></iframe>
+		<iframe ng-attr-src="{{ htmlPath }}" ng-if="type == 'html'" id="container" class="html" scrolling="yes" fullscreen-dir></iframe>
 		<div id="container" ng-if="type =='flash'"></div>
 	</div>
 	<div id="container" ng-if="type =='noflash'">
