e91411a5974f86326c4ae0d8767b5674fb85319f
Ian Turgeon <iturgeon@gmail.com>
2017-08-02 16:05:38 -0400

    fixes missing jqplot libs and a few other routes to local files

--- fuel/app/config/js.php
+++ fuel/app/config/js.php
@@ -5,22 +5,20 @@ return [
 	'paths' => [
 		'theme'  => '/themes/default/assets/js/',
 		'cdnjs' => '//cdnjs.cloudflare.com/ajax/libs/',
-		'lib' => '/assets/js/lib/',
-		'materia' => '//localhost:8008/assets/vendor/materia/js/',
-		'static' => '//localhost:8008/assets/',
+		'static' => '//localhost:8008/js/',
 	],
 
 	'groups' => [
-		'materia'    => ['materia::materia.js'],
+		'materia'    => ['static::materia.js'],
 		'angular'    => ['cdnjs::angular.js/1.3.0/angular.min.js'],
 		'ng_modal'   => ['static::vendor/ngmodal/ng-modal.min.js'],
 		'jquery'     => ['cdnjs::jquery/1.8.1/jquery.min.js'],
-		'author'     => ['materia::author.js'],
-		'student'    => ['materia::student.js'],
+		'author'     => ['static::author.js'],
+		'student'    => ['static::student.js'],
 		'dataTables' => ['cdnjs::datatables/1.10.7/js/jquery.dataTables.min.js'],
 		'jquery_ui'  => ['cdnjs::jqueryui/1.10.4/jquery-ui.min.js'],
 		'labjs'      => ['cdnjs::labjs/2.0.3/LAB.min.js'],
-		'spinner'    => ['static::vendor/spinjs/spin.min.js', 'static::spin.jquery.js'],
+		'spinner'    => ['static::vendor/spin.min.js', 'static::spin.jquery.js'],
 		'fancybox'   => ['static::vendor/fancybox/jquery.fancybox.pack.js'],
 		'swfobject'  => ['cdnjs::swfobject/2.2/swfobject.min.js'],
 
@@ -47,8 +45,8 @@ return [
 			'static::vendor/timepicker/jquery-ui-timepicker-addon.js',
 		],
 
-		'widget_play' => ['lib::jquery-ui-1.8.21.custom.min.js'],
+		'widget_play' => ['static::jquery-ui-1.8.21.custom.min.js'],
 
-		'lti_picker' => ['lib::jquery-ui-1.10.3.custom.min.js'],
+		'lti_picker' => ['static::jquery-ui-1.10.3.custom.min.js'],
 	],
 ];
