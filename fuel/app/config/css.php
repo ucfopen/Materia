185e4fa84dea75f3b0a92cd599929b5c2e7beede
Ian Turgeon <iturgeon@gmail.com>
2017-08-03 16:33:28 -0400

    adds dyanmic support for cache busiting assets in materia-client-assets

--- fuel/app/config/css.php
+++ fuel/app/config/css.php
@@ -1,11 +1,10 @@
 <?php
+$cdnjs = '//cdnjs.cloudflare.com/ajax/libs/';
+$static = \Config::get('materia.urls.static');
+$static_css = $static.'css/';
+$g_fonts = '//fonts.googleapis.com/';
+
 return [
-	'hash_file' => 'asset_hash.css.json',
-	'paths' => [
-		'gfonts' => '//fonts.googleapis.com/',
-		'cdnjs' => '//cdnjs.cloudflare.com/ajax/libs/',
-		'static' => '//localhost:8008/css/',
-	],
 
 	'always_load_groups' => [
 		'default' => [
@@ -16,75 +15,76 @@ return [
 
 	'groups' => [
 		'widget_play' => [
-			'static::play.css',
-			'static::../js/vendor/ngmodal/ng-modal.css'
+			$static_css.'play.css',
+			$static.'js/vendor/ngmodal/ng-modal.css'
 		],
 		'lti' => [
-			'static::main.css',
-			'static::lti.css',
+			$static_css.'main.css',
+			$static_css.'lti.css',
 		],
 		'my_widgets' => [
-			'static::my_widgets.css',
-			'cdnjs::jqPlot/1.0.0/jquery.jqplot.min.css',
-			'static::ui-lightness/jquery-ui-1.8.21.custom.css',
-			'static::ui-lightness/jquery-ui-timepicker-addon.css',
-			'static::jquery.dataTables.css',
-			'static::../js/vendor/ngmodal/ng-modal.css'
+			$static_css.'my_widgets.css',
+			$cdnjs.'jqPlot/1.0.0/jquery.jqplot.min.css',
+			$static_css.'ui-lightness/jquery-ui-1.8.21.custom.css',
+			$static_css.'ui-lightness/jquery-ui-timepicker-addon.css',
+			$static_css.'jquery.dataTables.css',
+			$static.'js/vendor/ngmodal/ng-modal.css'
 		],
 		'widget_editor' => [
-			'static::create.css',
-			'static::../js/vendor/ngmodal/ng-modal.css'
+			$static_css.'create.css',
+			$static.'js/vendor/ngmodal/ng-modal.css'
 		],
 		'widget_detail' => [
-			'cdnjs::fancybox/1.3.4/jquery.fancybox-1.3.4.css',
-			'static::widget.css',
+			$cdnjs.'fancybox/1.3.4/jquery.fancybox-1.3.4.css',
+			$static_css.'widget.css',
 		],
 		'widget_catalog' => [
-			'static::catalog.css',
+			$static_css.'catalog.css',
 		],
 		'profile' => [
-			'static::user.css',
+			$static_css.'user.css',
 		],
 		'login' => [
-			'static::login.css',
+			$static_css.'login.css',
 		],
 		'scores' => [
-			'cdnjs::jqPlot/1.0.0/jquery.jqplot.min.css',
-			'static::scores.css',
+			$cdnjs.'jqPlot/1.0.0/jquery.jqplot.min.css',
+			$static_css.'scores.css',
 		],
 		'embed_scores' => [
-			'static::scores.css',
+			$static_css.'scores.css',
 		],
 		'question_catalog' => [
-			'static::jquery.dataTables.css','static::question-import.css',
+			$static_css.'jquery.dataTables.css',
+			$static_css.'question-import.css',
 		],
 		'media_catalog' => [
-			'static::jquery.dataTables.css',
-			'cdnjs::plupload/1.5.4/jquery.plupload.queue/css/jquery.plupload.queue.css',
-			'static::media-import.css'
+			$static_css.'jquery.dataTables.css',
+			$cdnjs.'plupload/1.5.4/jquery.plupload.queue/css/jquery.plupload.queue.css',
+			$static_css.'media-import.css'
 		],
 		'homepage' => [
-			'static::store.css',
-			'static::widget.css',
+			$static_css.'store.css',
+			$static_css.'widget.css',
 		],
 		'help' => [
-			'static::docs.css',
+			$static_css.'docs.css',
 		],
 		'404' => [
-			'static::404.css',
+			$static_css.'404.css',
 		],
 		'500' => [
-			'static::500.css',
+			$static_css.'500.css',
 		],
 		'core' => [
-			'static::main.css',
+			$static_css.'main.css',
 		],
 		'upload' => [
-			'static::upload.css',
+			$static_css.'upload.css',
 		],
 		'fonts' => [
-			'gfonts::css?family=Kameron:700&text=0123456789%25',
-			'gfonts::css?family=Lato:300,400,700,700italic,900&amp;v2',
+			$g_fonts.'css?family=Kameron:700&text=0123456789%25',
+			$g_fonts.'css?family=Lato:300,400,700,700italic,900&amp;v2',
 		],
 	],
 ];
