92aeb445e80cbf98c72dee1a898f213c0ba456ee
Ian Turgeon <iturgeon@gmail.com>
2017-08-11 20:57:44 -0400

    changes auth.restrict_normal_logins config name and adds it to the install helper

--- fuel/packages/ltiauth/config/auth.php
+++ fuel/packages/ltiauth/config/auth.php
@@ -5,7 +5,7 @@ return [
 	'driver' => ['LtiAuth'],
 
 	// Use to control how non-admin users are allowed to log in
-	'restrict_normal_logins' => true,
+	'restrict_logins_to_lti_single_sign_on' => true,
 
 	// Use your own salt for security reasons
 	'salt' => 'CHANGE ME',
