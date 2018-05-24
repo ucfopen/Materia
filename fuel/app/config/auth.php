c3b63f4a96860294091f204db147be58f9a362f6
Ian Turgeon <iturgeon@gmail.com>
2018-05-24 17:42:29 -0400

    enables the login links in development

--- fuel/app/config/auth.php
+++ fuel/app/config/auth.php
@@ -6,6 +6,7 @@ return [
 		// Add auth drivers to use SAML, or something custom
 	],
 
+	'restrict_logins_to_lti_single_sign_on' => false,
 	// Use your own salt for security reasons
 	'salt' => 'SET THIS IN YOUR ENV CONFIG',
 ];
