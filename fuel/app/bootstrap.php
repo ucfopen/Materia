c041b7314bbccdda5cf8b46c3f8f4435660980e6
Ian Turgeon <iturgeon@gmail.com>
2018-06-22 19:24:29 -0400

    remove un-needed namesaces for newly moved namespaced code

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -7,8 +7,6 @@ define('PUBPATH', realpath(__DIR__.DS.'..'.DS.'..'.DS.'public').DS );
 define('STATICPATH', realpath(PUBPATH.DS.'..'.DS.'static').DS );
 define('MATERIAPATH', realpath(__DIR__.DS.'classes'.DS.'materia'));
 define('RDPATH', realpath(__DIR__.DS.'classes'.DS.'rocketduck'));
-\Autoloader::add_namespace('Materia', MATERIAPATH);
-\Autoloader::add_namespace('RocketDuck', RDPATH);
 \Autoloader::add_classes([
 	// FUELPHP classes
 	'Fuel\\Core\\Errorhandler'          => MATERIAPATH.DS.'errorhandler.php',
@@ -18,44 +16,7 @@ define('RDPATH', realpath(__DIR__.DS.'classes'.DS.'rocketduck'));
 	'Log'                               => RDPATH.'/fuel/core/log.php',
 	'TestCase'                          => RDPATH.'/fuel/core/testcase.php',
 
-	// ROCKETDUCK classes
-	// 'RocketDuck\\Db_Role'               => RDPATH.'/db/role.php',
-	// 'RocketDuck\\Log'                   => RDPATH.'/log.php',
-	// 'RocketDuck\\Msg'                   => RDPATH.'/msg.php',
-	// 'RocketDuck\\Perm_Acl'              => RDPATH.'/perm/acl.php',
-	// 'RocketDuck\\Perm_Manager'          => RDPATH.'/perm/manager.php',
-	// 'RocketDuck\\Perm_Role'             => RDPATH.'/perm/role.php',
-	// 'RocketDuck\\Util_Validator'        => RDPATH.'/util/validator.php',
-
-	// 'Materia\\api'                      => MATERIAPATH.DS.'api.php',
-	// 'Materia\\Api_v1'                   => MATERIAPATH.DS.'api/v1.php',
-	// 'Materia\\Community_manager'        => MATERIAPATH.DS.'community/manager.php',
-	// 'Materia\\message'                  => MATERIAPATH.DS.'message.php',
-	// 'Materia\\notification'             => MATERIAPATH.DS.'notification.php',
-	// 'Materia\\Notification_manager'     => MATERIAPATH.DS.'notification/manager.php',
-	// 'Materia\\perm'                     => MATERIAPATH.DS.'perm.php',
-	// 'Materia\\Perm_manager'             => MATERIAPATH.DS.'perm/manager.php',
-	// 'Materia\\Score_manager'            => MATERIAPATH.DS.'score/manager.php',
-	// 'Materia\\Score_record'             => MATERIAPATH.DS.'score/record.php',
-	// 'Materia\\Session_activity'         => MATERIAPATH.DS.'session/activity.php',
-	// 'Materia\\Session_log'              => MATERIAPATH.DS.'session/log.php',
-	// 'Materia\\Session_logger'           => MATERIAPATH.DS.'session/logger.php',
-	// 'Materia\\Session_play'             => MATERIAPATH.DS.'session/play.php',
-	// 'Materia\\Storage_manager'          => MATERIAPATH.DS.'storage/manager.php',
-	// 'Materia\\User_meta'                => MATERIAPATH.DS.'user/meta.php',
-	// 'Materia\\utils'                    => MATERIAPATH.DS.'utils.php',
-	// 'Materia\\widget'                   => MATERIAPATH.DS.'widget.php',
-	// 'Materia\\Widget_asset'             => MATERIAPATH.DS.'widget/asset.php',
-	// 'Materia\\Widget_Asset_manager'     => MATERIAPATH.DS.'widget/asset/manager.php',
-	// 'Materia\\Widget_instance'          => MATERIAPATH.DS.'widget/instance.php',
-	// 'Materia\\Widget_Instance_hash'     => MATERIAPATH.DS.'widget/instance/hash.php',
-	// 'Materia\\Widget_Instance_manager'  => MATERIAPATH.DS.'widget/instance/manager.php',
-	// 'Materia\\Widget_manager'           => MATERIAPATH.DS.'widget/manager.php',
-	// 'Materia\\Widget_question'          => MATERIAPATH.DS.'widget/question.php',
-	// 'Materia\\Widget_Question_answer'   => MATERIAPATH.DS.'widget/question/answer.php',
-	// 'Materia\\Widget_Question_manager'  => MATERIAPATH.DS.'widget/question/manager.php',
-	// 'Materia\\Widget_Question_Type_mc'  => MATERIAPATH.DS.'widget/question/type/mc.php',
-	// 'Materia\\Widget_Question_Type_qa'  => MATERIAPATH.DS.'widget/question/type/qa.php',
+	// TODO: build task that will resolve/populate all the classes in materia here
 ]);
 
 
@@ -84,6 +45,7 @@ if(\FUEL::$env === \FUEL::TEST){
 \Fuel::init('config.php');
 
 // register events after the app is initialized
+// if this is placed in config/events, it'll load notification before orm gets loaded :(
 \Event::register('delete_widget_event', '\Model_Notification::on_widget_delete_event');
 
 // A function to trace stuffs to the log
