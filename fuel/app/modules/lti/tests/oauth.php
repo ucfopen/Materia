3fbb69ce895de82fb32960c33a4557c9ae2e7257
Ian Turgeon <iturgeon@gmail.com>
2018-06-22 19:15:08 -0400

    fixes tests for materia widgets after moving the test widget

--- fuel/app/modules/lti/tests/oauth.php
+++ fuel/app/modules/lti/tests/oauth.php
@@ -9,7 +9,7 @@ class Test_Oauth extends \Test_Basetest
 {
 	public function test_oauth_validate_passes_when_expected()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
 		$this->create_test_oauth_launch([], \Uri::current(), $user);
@@ -20,7 +20,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_no_signature()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
 		$this->create_test_oauth_launch([], \Uri::current(), $user);
@@ -31,7 +31,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_no_oauth_timestamp()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
 		$this->create_test_oauth_launch([], \Uri::current(), $user);
@@ -42,7 +42,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_no_oauth_nonce()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
 		$this->create_test_oauth_launch([], \Uri::current(), $user);
@@ -53,7 +53,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_no_oauth_consumer_key()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
 		$this->create_test_oauth_launch([], \Uri::current(), $user);
@@ -64,7 +64,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_an_old_signature()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 		$lti_config = \Config::get("lti::lti.consumers.".\Input::post('tool_consumer_info_product_family_code', 'default'));
 
 		$user_id = \Fuel\Tasks\Admin::instant_user();
@@ -77,7 +77,7 @@ class Test_Oauth extends \Test_Basetest
 
 	public function test_oauth_validate_fails_with_an_incorrect_signature()
 	{
-		require_once(PKGPATH.'materia/tasks/admin.php');
+		require_once(APPPATH.'/tasks/admin.php');
 
 		$user_id = \Fuel\Tasks\Admin::instant_user();
 		$user = \Model_User::find($user_id);
