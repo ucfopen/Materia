d72ceb997a6f3e2f89564805f533e1feb060e369
Ian Turgeon <iturgeon@gmail.com>
2018-06-25 10:58:34 -0400

    removes materia it's a thing header

--- fuel/app/classes/materia/semester.php
+++ fuel/app/classes/materia/semester.php
@@ -1,17 +1,5 @@
 <?php
 /**
- * Materia
- * It's a thing
- *
- * @package	    Materia
- * @version    1.0
- * @author     UCF New Media
- * @copyright  2011 New Media
- * @link       http://kogneato.com
- */
-
-
-/**
  * Model for the Semesters
  *
  * The widget managers for the Materia package.
