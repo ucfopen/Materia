62acaff53ee635c0a49e6ad551a0b56d1d2f4a2d
Ian Turgeon <iturgeon@gmail.com>
2018-09-27 00:46:04 -0400

    Widget install overhaul to allow heroku build to install them
    Fix heroku app.json FUEL_ENV definition
    adds composer script heroku-extract-widgets
    changes a bunch of stuff in widget installer to allow files to be unpacked without touching the db
    adds some php 7.1 function defs
    adds prefer-stable flag to composer
    adds ext-mbstring reqirement for crossword
    adds heroku configs for lti and crytpo stuff
    adds support for user pw env
    adds support for cleardb and jawsdb
    
    adds widget tasks:
    * install_from_url
    * extact_from_config
    * extract_without_install (renamed from extract)

--- fuel/app/config/heroku/config.php
+++ fuel/app/config/heroku/config.php
@@ -1,8 +1,36 @@
 <?php
+// all files in heroku/config are copied to config/production on heroku
+// they will overwrite any files already in config/production
 return [
 	'log_threshold'    => Fuel::L_DEBUG,
 	# send logs via STDOUT for heroku
 	'log_handler_factory'   => function($locals, $level){ return new \Monolog\Handler\ErrorLogHandler(); },
 
-	'enable_uploader' => ($_ENV['MATERIA_ENABLE_UPLOADER'] === 'true')
+	'widgets' => [
+		[
+			'id' => 1,
+			'package'  => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/crossword_1a9a888e5d540e4a8eeea6575abf6ea1170bba50.wigt',
+			'checksum' => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/crossword_1a9a888e5d540e4a8eeea6575abf6ea1170bba50.wigt.yml',
+		],
+		[
+			'id' => 2,
+			'package'  => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/hangman_8150b16085050dde81a7c727b8b946a679082895.wigt',
+			'checksum' => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/hangman_8150b16085050dde81a7c727b8b946a679082895.wigt.yml',
+		],
+		[
+			'id' => 3,
+			'package'  => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/matching_e7b6fa775eb54427045fff136a33c179affc681d.wigt',
+			'checksum' => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/matching_e7b6fa775eb54427045fff136a33c179affc681d.wigt.yml',
+		],
+		[
+			'id' => 4,
+			'package'  => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/enigma_a149e18d93df78ef775665ffdc02b6490f8faad5.wigt',
+			'checksum' => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/enigma_a149e18d93df78ef775665ffdc02b6490f8faad5.wigt.yml',
+		],
+		[
+			'id' => 5,
+			'package'  => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/adventure_66631aca57d255eb61854b02b14c63604d8e8a66.wigt',
+			'checksum' => 'https://ucfcdl-deploy-builds-public.s3.amazonaws.com/materia-widgets/adventure_66631aca57d255eb61854b02b14c63604d8e8a66.wigt.yml',
+		],
+	],
 ];
