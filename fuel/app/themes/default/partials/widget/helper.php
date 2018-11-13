c48be813dc560c00a782568f8aecff338d3cf8b6
Tuan Pham <Tuan.Pham@ucf.edu>
2018-11-13 13:58:32 -0500

    add guide controller, update detail partials with guides

--- fuel/app/themes/default/partials/widget/helper.php
+++
@@ -1,6 +0,0 @@
-<div ng-controller="createGuideCtrl">
-	<!-- <div class="center">
-		<iframe ng-attr-src="{{ helper }}" id="container" class="html"></iframe>
-	</div> -->
-	<iframe ng-attr-src="{{ helper }}" ng-class="{ show: helper }" id="embed_dialog" frameborder=0 width=675 height=500></iframe>
-</div>
