343c37bc6018f6ad267251d079c6ebb0cc061be7
Nathan Dabu <nathan.dabu@ucf.edu>
2019-02-07 14:03:58 -0500

    add 'featured' label for 'in_catalog' widgets on catalog page

--- fuel/app/themes/default/partials/widget/catalog.php
+++ fuel/app/themes/default/partials/widget/catalog.php
@@ -48,7 +48,11 @@
 
 				<a class="infocard" ng-href="/widgets/{{widget.id}}-{{widget.clean_name}}">
 					<div class="header">
-						<h1 class="infoHeader">{{widget.name}}</h1>
+						<div class="featured-label" ng-if="widget.in_catalog == '1'">
+							<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/><path fill="none" d="M0 0h18v18H0z"/></svg>
+							Featured
+						</div>
+						<h1 class="infoHeader" ng-class="{featured: widget.in_catalog == '1'}">{{widget.name}}</h1>
 					</div>
 					<div class="img-holder">
 						<img ng-src='{{widget.icon}}'>
