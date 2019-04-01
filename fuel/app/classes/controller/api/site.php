0ef3d253d87238e98caed086f2742d0664e8ee22
Ian Turgeon <iturgeon@gmail.com>
2019-04-01 12:01:33 -0400

    updates result format for health_status api endpoint

--- fuel/app/classes/controller/api/site.php
+++ fuel/app/classes/controller/api/site.php
@@ -13,6 +13,7 @@ class Controller_Api_Site extends Controller_Rest
 
 	public function get_health_status()
 	{
+		// setup the rate limiter
 		$this->no_cache();
 
 		static::$rate_limiter_down_time = 3600; // seconds
@@ -26,31 +27,38 @@ class Controller_Api_Site extends Controller_Rest
 			return;
 		}
 
-		$tests = [
-			'database' => false,
-			'cache' => false,
-			'session' => false,
-			'assets' => false,
-			'authSalt' => false,
-			'diskspace' => false,
+		// rate limiter passed, do checks
+
+		$result = [
+			'tool'    => 'Materia Server',
+			'url'     => \Uri::base(false),
+			'healthy' => false,
+			'checks'  => [
+				'database'  => false,
+				'cache'     => false,
+				'session'   => false,
+				'assets'    => false,
+				'authSalt'  => false,
+				'diskspace' => false,
+			],
 		];
 
 		// test database connection
 		$q = \DB::query('SELECT 1 as good_to_go')->execute()->as_array();
-		$tests['database'] = ! empty($q[0]['good_to_go']);
+		$result['checks']['database'] = ! empty($q[0]['good_to_go']);
 
 		// test cache set and get
 		$random = uniqid();
 		\Cache::set('health-status', $random, 300);
 		$cache_value = \Cache::easy_get('health-status');
-		$tests['cache'] = $cache_value === $random;
+		$result['checks']['cache'] = $cache_value === $random;
 
 		\Session::set('health-status', $random);
 		$session_value = \Session::get('health-status');
-		$tests['session'] = $session_value === $random;
+		$result['checks']['session'] = $session_value === $random;
 
 
-		if ($tests['database'])
+		if ($result['checks']['database'])
 		{
 			$q = \Db::query('SELECT id from asset order by RAND() LIMIT 1')
 				->execute()
@@ -58,16 +66,24 @@ class Controller_Api_Site extends Controller_Rest
 
 			$random_asset = \Materia\Widget_Asset::fetch_by_id($q[0]['id']);
 			$path = $random_asset->copy_asset_to_temp_file($random_asset->id, 'original');
-			$tests['assets'] = filesize($path) > 1;
+			$result['checks']['assets'] = filesize($path) > 1;
 			unset($path);
 		}
 
 		\Config::load('auth', true);
-		$tests['authSalt'] = \Config::get('auth.salt', 'error') != 'SET THIS IN YOUR ENV CONFIG';
+		$result['checks']['authSalt'] = \Config::get('auth.salt', 'error') != 'SET THIS IN YOUR ENV CONFIG';
 
 
-		$tests['diskspace'] = disk_free_space('.') > (200 /*mb*/ * 1024 * 1024);
-		$this->response($tests, 200);
+		$result['checks']['diskspace'] = disk_free_space('.') > (200 /*mb*/ * 1024 * 1024);
+
+		// gather the values of each check
+		$reduce_truth = function($carry, $item){
+			return $item && $carry;
+		};
+
+		$result['healthy'] = array_reduce($result['checks'], $reduce_truth, true);
+
+		$this->response($result, 200);
 	}
 
 }
