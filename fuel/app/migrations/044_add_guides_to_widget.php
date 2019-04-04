b2928636acccd741ae40ba0bfcc7ad06197ec6f1
Nathan Dabu <nathan.dabu@ucf.edu>
2019-04-04 13:40:51 -0400

    spaces > tabs in add_guides_to_widget migration

--- fuel/app/migrations/044_add_guides_to_widget.php
+++ fuel/app/migrations/044_add_guides_to_widget.php
@@ -4,28 +4,28 @@ namespace Fuel\Migrations;
 
 class Add_Guides_To_Widget
 {
-    public function up()
-    {
-        \DBUtil::add_fields(
-            'widget',
-            [
-                'creator_guide' => [
-                    'constraint' => 255, 
-                    'type' => 'varchar',
-                    'null' => false
-                ],
-                'player_guide' => [
-                    'constraint' => 255, 
-                    'type' => 'varchar',
-                    'null' => false
-                ]
-            ]
-        );
-    }
+	public function up()
+	{
+		\DBUtil::add_fields(
+			'widget',
+			[
+				'creator_guide' => [
+					'constraint' => 255,
+					'type' => 'varchar',
+					'null' => false
+				],
+				'player_guide' => [
+					'constraint' => 255,
+					'type' => 'varchar',
+					'null' => false
+				]
+			]
+		);
+	}
 
-    public function down()
-    {
-        \DBUtil::drop_fields('widget', ['creator_guide', 'player_guide']);
-        
-    }
+	public function down()
+	{
+		\DBUtil::drop_fields('widget', ['creator_guide', 'player_guide']);
+
+	}
 }
