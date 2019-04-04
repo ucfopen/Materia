3736bb7acba419b8bed1c36ca563ff769eca8409
Nathan Dabu <nathan.dabu@ucf.edu>
2019-04-04 16:37:53 -0400

    added breadcrumb to widget details page

--- fuel/app/themes/default/partials/widget/detail.php
+++ fuel/app/themes/default/partials/widget/detail.php
@@ -1,4 +1,9 @@
 <section class="page" ng-show="show" ng-controller="widgetDetailsController" ng-cloak ng-style="{'max-width': maxPageWidth}">
+	<div id="breadcrumb-container">
+		<div class="breadcrumb"><a href="/widgets">Widget Catalog</a></div>
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+		<div class="breadcrumb">{{ widget.name }}</div>
+	</div>
 	<article class="widget_detail">
 		<div class="top">
 			<img ng-src="{{ widget.icon }}" alt="" class="widget_icon">
