df450a9fca29aecd53ea67ffcedb28cd7aa7c501
Davis Goff <davis.goff@ucf.edu>
2019-10-15 11:17:08 -0400

    Add inline info about previewing widgets and student view

--- fuel/app/themes/default/lti/partials/open_preview.php
+++ fuel/app/themes/default/lti/partials/open_preview.php
@@ -8,4 +8,9 @@
 		<p>Students will see the widget instead of this message.</p>
 		<a target="_blank" class="button" href="<?= $preview_url ?>">Preview widget in new tab</a>
 	</div>
+
+	<div class="help-container">
+		<p>You can preview the widget if you have access to it.</p>
+		<p>To view the widget in Canvas as a student, view the assignment while in Student View. <a href="https://community.canvaslms.com/docs/DOC-13122-415261153" target="_blank" class="external">See how!</a></p>
+	</div>
 </section>
