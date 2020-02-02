9d401f8bb21be9b1a7fb4689f2e20dc7810218d4
Ian Turgeon <iturgeon@gmail.com>
2020-02-02 17:37:50 -0500

    upgrade default sort-it-out and this-or-that

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -351,8 +351,8 @@ return array(
 		],
 		[
 			'id' => 7,
-			'package' => 'https://github.com/ucfopen/this-or-that-materia-widget/releases/download/v1.0.8/this-or-that.wigt',
-			'checksum' => 'https://github.com/ucfopen/this-or-that-materia-widget/releases/download/v1.0.8/this-or-that-build-info.yml'
+			'package' => 'https://github.com/ucfopen/this-or-that-materia-widget/releases/download/v1.0.9/this-or-that.wigt',
+			'checksum' => 'https://github.com/ucfopen/this-or-that-materia-widget/releases/download/v1.0.9/this-or-that-build-info.yml'
 		],
 		[
 			'id' => 8,
@@ -371,8 +371,8 @@ return array(
 		],
 		[
 			'id' => 11,
-			'package' => 'https://github.com/ucfopen/sort-it-out-materia-widget/releases/download/v1.0.4/sort-it-out.wigt',
-			'checksum' => 'https://github.com/ucfopen/sort-it-out-materia-widget/releases/download/v1.0.4/sort-it-out-build-info.yml'
+			'package' => 'https://github.com/ucfopen/sort-it-out-materia-widget/releases/download/v1.0.6/sort-it-out.wigt',
+			'checksum' => 'https://github.com/ucfopen/sort-it-out-materia-widget/releases/download/v1.0.6/sort-it-out-build-info.yml'
 		],
 		[
 			'id' => 12,
