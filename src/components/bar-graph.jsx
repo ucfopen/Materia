fdd92bf7a87910cc3f200bbc6e35a903a9387195
iturgeon <iturgeon@gmail.com>
2020-08-17 23:59:32 -0400

    [wip] splitting up react my-widget scores graphs

--- src/components/bar-graph.jsx
+++ src/components/bar-graph.jsx
@@ -3,11 +3,12 @@ import { Bar } from '@vx/shape';
 import { Group } from "@vx/group"
 import { scaleBand, scaleLinear } from "@vx/scale"
 import { AxisLeft, AxisBottom } from "@vx/axis"
+import { Grid } from '@vx/grid'
 
 
 // accessors return the label and value of that data item
-const x = (d) => d.label
-const y = (d) => d.value
+const x = d => d.label
+const y = d => d.value
 
 const BarChart = ({ data, width, height }) => {
 	// bounds
@@ -28,12 +29,22 @@ const BarChart = ({ data, width, height }) => {
 
 	return (
 		<svg width={width} height={height}>
-			<Group top={25} left={55}>
-				<AxisLeft left={10} scale={yScale} numTicks={4} label="Plays" />
+			<Group top={25} left={40}>
+				<Grid
+					left={13}
+					bottom={15}
+					xScale={xScale}
+					yScale={yScale}
+					width={xMax-10}
+					height={yMax}
+					numTicksRows={4}
+					numTicksColumns={11}
+				/>
+				<AxisLeft left={13} scale={yScale} numTicks={4} label="Plays" />
 				{data.map((d, i) => {
-					const label = x(d)
+					const label = d.label
 					const barWidth = xScale.bandwidth()
-					const barHeight = yMax - yScale(y(d))
+					const barHeight = yMax - yScale(d.value)
 					const barX = xScale(label)
 					const barY = yMax - barHeight
 					return <Bar key={`bar-${label}`} x={barX} y={barY} width={barWidth} height={barHeight} />
--- src/components/my-widgets-scores.jsx
+++ src/components/my-widgets-scores.jsx
@@ -1,11 +1,192 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import BarGraph from './bar-graph'
+
+const MyWidgetScoreSemesterTable = ({semester}) => {
+	return (
+		<div score-table
+			className="display table"
+			id={`table_${semester.id}`}
+			data-term={semester.term}
+			data-year={semester.year}
+		>
+			<div className="score-search">
+				<input
+					type="text"
+					ng-model="studentSearch"
+					ng-change="searchStudentActivity(studentSearch)"
+					placeholder="Search Students" />
+			</div>
+			<h3>Select a student to view their scores.</h3>
+			<div className="scoreListContainer">
+				<div className="scoreListScrollContainer">
+					<table className="scoreListTable">
+						<tbody>
+
+							<tr ng-repeat="user in users track by user.uid"
+								id="{{$index}}"
+								ng-className="{'rowSelected' : user.uid == selectedUser.uid}">
+								<td className="listName"
+									ng-click="setSelectedUser(user.uid)">
+									{user.name}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div className="scoreTableContainer"
+				ng-hide="selectedUser == null">
+				<table className="scoreTable">
+					<tbody>
+						<tr ng-repeat="score in selectedUser.scores"
+							ng-click="showScorePage(score.id)">
+							<td>{ score.date.substring(0, 10) }</td>
+							<td>{ score.complete == "1" ? score.percent + "%" : "---" }</td>
+							<td>{ score.elapsed }</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
+}
+
+const MyWidgetScoreSemesterStorage = ({semester}) => {
+	return (
+		<div
+			score-data id="data_{{semester.id}}"
+			className="display data"
+			data-semester="{{semester.year}} {{semester.term.toLowerCase()}}"
+			data-has-storage="{{ semester.storage ? true : false }}"
+			ng-if="selectedScoreView[$index] == SCORE_TAB_STORAGE"
+		>
+			<div>
+				<input type='checkbox'
+					ng-model='semester.anonymize'
+					ng-init='semester.anonymize=false' />
+				Anonymize Download
+				<a className="storage"
+					ng-href="/data/export/{{inst.id}}?type=storage&amp;table={{selectedTable | escape}}&amp;semesters={{semester.year}}-{{semester.term}}&amp;anonymized={{semester.anonymize}}" >
+					Download Table
+				</a>
+			</div>
+			<div className="table label"
+				ng-show="tableNames.length == 1">
+				<h4>Table:
+					<span>{ tableNames[0] }</span>
+				</h4>
+			</div>
+			<select ng-model="selectedTable"
+				ng-options="tableName as tableName for tableName in tableNames"
+				ng-show="tableNames.length > 1">
+			</select>
+			<div ng-repeat="table in tables"
+				ng-show="tableNames[$index] == selectedTable">
+				<p ng-if="table.truncated"
+					className="truncated-table">
+					Showing only the first { MAX_ROWS } entries of this table.
+					Download the table to see all entries.
+				</p>
+				<table className="storage_table"
+					datatable>
+					<thead>
+						<tr>
+							<th>user</th>
+							<th>firstName</th>
+							<th>lastName</th>
+							<th>time</th>
+							<th ng-repeat="(name, data) in table.data[0].data">{{name}}</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr ng-repeat="row in table.data">
+							<td>{ row.play.user }</td>
+							<td>{ row.play.firstName }</td>
+							<td>{ row.play.lastName }</td>
+							<td>{ row.play.cleanTime }</td>
+							<td ng-repeat="rowData in row.data"
+								ng-className="{'null':rowData == null}">
+								{ rowData }
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	)
+}
+
+const MyWidgetScoreSemester = ({semester}) => {
+	const [scoreTab, setScoreTab] = useState('SCORE_GRAPH_TAB')
+
+	const activeTab = useMemo(() => {
+			switch(scoreTab){
+				case 'SCORE_GRAPH_TAB':
+					return (
+						<div className="display graph">
+							<BarGraph data={semester.graphData} width="530" height="300" />
+						</div>
+					)
+
+				case 'SCORE_STORAGE_TAB':
+					return <MyWidgetScoreSemesterTable semester={semester} />
+
+				case 'SCORE_STORAGE_TAB':
+					return <MyWidgetScoreSemesterStorage semester={semester}/>
+			}
+		}, [scoreTab, semester]
+	)
+
+	return (
+		<div key={semester} className="scoreWrapper">
+			<h3 className="view">{semester.term} {semester.year}</h3>
+			<ul className="choices">
+				<li className={scoreTab == 'SCORE_GRAPH_TAB' ? 'scoreTypeSelected' : ''}>
+					<a className="graph" onClick={() => {setScoreTab('SCORE_GRAPH_TAB')}}>
+						Graph
+					</a>
+				</li>
+				<li className={scoreTab == 'SCORE_TABLE_TAB' ? 'scoreTypeSelected' : ''}>
+					<a className="table" onClick={() => {setScoreTab('SCORE_TAB_INDIVIDUAL')}}>
+						Individual Scores
+					</a>
+				</li>
+				<li className={scoreTab == 'SCORE_STORAGE_TAB' ? 'scoreTypeSelected' : ''}>
+					<a className="data" onClick={() => {setScoreTab('SCORE_TAB_STORAGE')}}>
+						Data
+					</a>
+				</li>
+			</ul>
+
+			{activeTab}
+
+			<ul className="numeric"
+				ng-show="selectedScoreView[$index] != SCORE_TAB_STORAGE">
+				<li>
+					<h4>Students</h4>
+					<p className="players"
+						className="playerShrink">
+						{ semester.students }
+					</p>
+				</li>
+				<li>
+					<h4>Scores</h4>
+					<p className="score-count">{ semester.totalScores }</p>
+				</li>
+				<li>
+					<h4>Avg Final Score</h4>
+					<p className="final-average">{ semester.average }</p>
+				</li>
+			</ul>
+		</div>
+	)
+}
 
 
 const MyWidgetsScores = ({inst}) => {
-	const [scoreTab, setScoreTab] = useState('SCORE_GRAPH_TAB')
 	const [scores, setScores] = useState([])
-	const [isLoadingScores, setIsLoadingScores] = useState(false)
+	const [isShowingAll, setIsShowingAll] = useState(false)
+	const [isLoadingScores, setIsLoadingScores] = useState(true)
 	useEffect(
 		() => {
 			setIsLoadingScores(true)
@@ -52,189 +233,33 @@ const MyWidgetsScores = ({inst}) => {
 		}, [inst.id]
 	)
 
+
+	const displayedScores = isShowingAll
+		? scores
+		: scores.slice(0,1) // gracefully handles empty array
+
 	return (
 		<div className="scores">
 			<h2>Student Activity</h2>
-			<span id="export_scores_button"
+			<span
+				id="export_scores_button"
 				className={`action_button aux_button ${scores.length ? '' : 'disabled'}`}
-				ng-click="selected.scores.list.length === NULL ? angular.noop() : exportPopup()">
+			>
 				<span className="arrow_down"></span>
 				Export Options
 			</span>
-
-
-			{scores.map(semester =>
-				<div key={semester} className="scoreWrapper">
-					<h3 className="view">{semester.term} {semester.year}</h3>
-					<ul className="choices">
-						<li className={scoreTab == 'SCORE_GRAPH_TAB' ? 'scoreTypeSelected' : ''}>
-							<a className="graph"
-								ng-show="semester.distribution"
-								ng-click="setScoreViewTab($index, SCORE_TAB_GRAPH)">
-								Graph
-							</a>
-						</li>
-						<li className={scoreTab == 'SCORE_TABLE_TAB' ? 'scoreTypeSelected' : ''}>
-							<a className="table"
-								ng-show="semester.distribution"
-								ng-click="setScoreViewTab($index, SCORE_TAB_INDIVIDUAL)">
-								Individual Scores
-							</a>
-						</li>
-						<li className={scoreTab == 'SCORE_STORAGE_TAB' ? 'scoreTypeSelected' : ''}>
-							<a className="data"
-								ng-show="semester.storage"
-								ng-click="setScoreViewTab($index, SCORE_TAB_STORAGE)">
-								Data
-							</a>
-						</li>
-					</ul>
-
-					{scoreTab == 'SCORE_GRAPH_TAB'
-						? <div className="display graph">
-								<BarGraph data={semester.graphData} width="530" height="300" />
-							</div>
-						: null
-					}
-
-					{scoreTab == 'SCORE_TABLE_TAB'
-						? <div score-table
-								className="display table"
-								id={`table_${semester.id}`}
-								data-term={semester.term}
-								data-year={semester.year}
-							>
-								<div className="score-search">
-									<input
-										type="text"
-										ng-model="studentSearch"
-										ng-change="searchStudentActivity(studentSearch)"
-										placeholder="Search Students" />
-								</div>
-								<h3>Select a student to view their scores.</h3>
-								<div className="scoreListContainer">
-									<div className="scoreListScrollContainer">
-										<table className="scoreListTable">
-											<tbody>
-
-												<tr ng-repeat="user in users track by user.uid"
-													id="{{$index}}"
-													ng-className="{'rowSelected' : user.uid == selectedUser.uid}">
-													<td className="listName"
-														ng-click="setSelectedUser(user.uid)">
-														{user.name}
-													</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-								<div className="scoreTableContainer"
-									ng-hide="selectedUser == null">
-									<table className="scoreTable">
-										<tbody>
-											<tr ng-repeat="score in selectedUser.scores"
-												ng-click="showScorePage(score.id)">
-												<td>{ score.date.substring(0, 10) }</td>
-												<td>{ score.complete == "1" ? score.percent + "%" : "---" }</td>
-												<td>{ score.elapsed }</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						: null
-					}
-
-					{scoreTab == 'SCORE_STORAGE_TAB'
-						? <div
-								score-data id="data_{{semester.id}}"
-								className="display data"
-								data-semester="{{semester.year}} {{semester.term.toLowerCase()}}"
-								data-has-storage="{{ semester.storage ? true : false }}"
-								ng-if="selectedScoreView[$index] == SCORE_TAB_STORAGE">
-								<div>
-									<input type='checkbox'
-										ng-model='semester.anonymize'
-										ng-init='semester.anonymize=false' />
-									Anonymize Download
-									<a className="storage"
-										ng-href="/data/export/{{inst.id}}?type=storage&amp;table={{selectedTable | escape}}&amp;semesters={{semester.year}}-{{semester.term}}&amp;anonymized={{semester.anonymize}}" >
-										Download Table
-									</a>
-								</div>
-								<div className="table label"
-									ng-show="tableNames.length == 1">
-									<h4>Table:
-										<span>{ tableNames[0] }</span>
-									</h4>
-								</div>
-								<select ng-model="selectedTable"
-									ng-options="tableName as tableName for tableName in tableNames"
-									ng-show="tableNames.length > 1">
-								</select>
-								<div ng-repeat="table in tables"
-									ng-show="tableNames[$index] == selectedTable">
-									<p ng-if="table.truncated"
-										className="truncated-table">
-										Showing only the first { MAX_ROWS } entries of this table.
-										Download the table to see all entries.
-									</p>
-									<table className="storage_table"
-										datatable>
-										<thead>
-											<tr>
-												<th>user</th>
-												<th>firstName</th>
-												<th>lastName</th>
-												<th>time</th>
-												<th ng-repeat="(name, data) in table.data[0].data">{{name}}</th>
-											</tr>
-										</thead>
-										<tbody>
-											<tr ng-repeat="row in table.data">
-												<td>{ row.play.user }</td>
-												<td>{ row.play.firstName }</td>
-												<td>{ row.play.lastName }</td>
-												<td>{ row.play.cleanTime }</td>
-												<td ng-repeat="rowData in row.data"
-													ng-className="{'null':rowData == null}">
-													{ rowData }
-												</td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						:null
-					}
-
-					<ul className="numeric"
-						ng-show="selectedScoreView[$index] != SCORE_TAB_STORAGE">
-						<li>
-							<h4>Students</h4>
-							<p className="players"
-								className="playerShrink">
-								{ semester.students }
-							</p>
-						</li>
-						<li>
-							<h4>Scores</h4>
-							<p className="score-count">{ semester.totalScores }</p>
-						</li>
-						<li>
-							<h4>Avg Final Score</h4>
-							<p className="final-average">{ semester.average }</p>
-						</li>
-					</ul>
-					<a role="button"
+			{displayedScores.map(semester => <MyWidgetScoreSemester semester={semester} />)}
+			{scores.length > 0 && !isShowingAll
+				?	<a role="button"
 						className="show-older-scores-button"
-						ng-show="selected.scores.list.length > 1 && show.olderScores == false && $index == 0"
-						ng-click="enableOlderScores()">
+						onClick={() => setIsShowingAll(true)}
+					>
+
 						Show older scores...
 					</a>
-				</div>
-			)}
+				: null
+			}
+
 		</div>
 	)
 }
