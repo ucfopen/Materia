001e43f51756c39c39bdac1632c730fd1dc33ab1
Corey Peterson <corey.peterson@ucf.edu>
2020-10-22 14:47:14 -0400

    Replaced playable_by_current_user with more specific user_has_any_perm_to check for history api call

--- fuel/app/classes/controller/api/instance.php
+++ fuel/app/classes/controller/api/instance.php
@@ -22,7 +22,7 @@ class Controller_Api_Instance extends Controller_Rest
 		if ( ! $inst_id = Input::get('inst_id')) return $this->response('Requires an inst_id parameter!', 401);
 		if ( ! \Materia\Util_Validator::is_valid_hash($inst_id) ) return $this->response(\Materia\Msg::invalid_input($inst_id), 401);
 		if ( ! ($inst = \Materia\Widget_Instance_Manager::get($inst_id))) return $this->response('Instance not found', 404);
-		if ( ! $inst->playable_by_current_user()) return $this->response(\Materia\Msg::no_login(), 401);
+		if ( ! \Materia\Perm_Manager::user_has_any_perm_to(\Model_User::find_current_id(), $inst_id, \Materia\Perm::INSTANCE, [\Materia\Perm::FULL])) return $this->response(\Materia\Msg::no_login(), 401);
 		
 		$history = $inst->get_qset_history($inst_id);
 
