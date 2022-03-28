a06bd53cf02bed17beb1810315c38bf2dd5d158c
Lawrence Oks <ljoks0727@gmail.com>
2022-03-28 13:03:42 -0400

    adds tar -k flag to setup script to as not to overwrite existing .gitignore in h5p/core and h5p/editor

--- h5p-server/setup.sh
+++ h5p-server/setup.sh
@@ -1,11 +1,11 @@
 curl -o h5p.zip https://codeload.github.com/h5p/h5p-php-library/zip/1.24.0
 unzip h5p.zip
-(cd h5p-php-library-1.24.0 && tar c .) | (cd h5p/core && tar xf -)
+(cd h5p-php-library-1.24.0 && tar c .) | (cd h5p/core && tar xfk -)
 rm -rf h5p.zip h5p-php-library-1.24.0
 
 curl -o editor.zip https://codeload.github.com/h5p/h5p-editor-php-library/zip/1.24.0
 unzip editor.zip
-(cd h5p-editor-php-library-1.24.0 && tar c .) | (cd h5p/editor && tar xf -)
+(cd h5p-editor-php-library-1.24.0 && tar c .) | (cd h5p/editor && tar xfk -)
 rm -rf editor.zip h5p-editor-php-library-1.24.0
 
 curl -o libraries.zip https://h5p.org/sites/default/files/h5p/exports/interactive-video-2-618.h5p
