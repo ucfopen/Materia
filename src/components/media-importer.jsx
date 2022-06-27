df7e2ec91457e536e0d8f99e625b26b226d02b36
Emanuel <emanuelcodes@gmail.com>
2022-06-27 15:07:12 -0400

    Add func for filtering files by using search a search bar. At the moment it is slow.

--- src/components/media-importer.jsx
+++ src/components/media-importer.jsx
@@ -4,76 +4,39 @@ import DragAndDrop from './drag-and-drop'
 import { getAllAssets, uploadFile, loadPickedAsset, onCancel } from '../util/media-importer'
 import './media.scss'
 
-const SORTING_NONE = false
-const SORTING_ASC = 'asc'
-const SORTING_DESC = 'desc'
-
 const sortString = (field, a, b) => a[field].toLowerCase().localeCompare(b[field].toLowerCase())
 const sortNumber = (field, a, b) => a[field] - b[field]
 
 const SORT_OPTIONS = [
 	{
 		sortMethod: sortString.bind(null, 'name'), // bind the field name to the sort method
-		name: 'Name',
+		name: 'Title',
 		field: 'name',
-		status: SORTING_ASC,
 	},
 	{
 		sortMethod: sortString.bind(null, 'type'), // bind the field name to the sort method
 		name: 'Type',
 		field: 'type',
-		status: SORTING_NONE,
 	},
 	{
 		sortMethod: sortNumber.bind(null, 'timestamp'), // bind the field name to the sort method
 		name: 'Date',
 		field: 'timestamp',
-		status: SORTING_NONE,
 	},
 ]
 
 const MediaImporter = () => {
 	const mounted = useRef(false)
-	const [sortAssets, setSortAssets] = useState(null)
-	const [sortOrder, setSortOrder] = useState(0)
-	const [numOfAssets, setNumOfAssets] = useState(0)
+	const [sortAssets, setSortAssets] = useState(null) // Display assets list
+	const [sortState, setSortState] = useState({
+		sortAsc: false, // Sorted list in asc or desc
+		sortOrder: 0, // List sorting options
+	})
+	const [sortedList, setSortedList] = useState([])
+
+	const [filterSearch, setFilterSearch] = useState('') // Search bar filter
+	const [numOfAssets, setNumOfAssets] = useState(0) // Number of Assets fetched
 	const { data: listOfAssets, isSuccess } = useQuery('assets', getAllAssets)
-
-	const sortAssetList = (sortType) => {
-		switch (sortType) {
-			case SORT_OPTIONS[0].name:
-				setSortOrder(0)
-				break
-
-			case SORT_OPTIONS[1].name:
-				setSortOrder(1)
-				break
-
-			case SORT_OPTIONS[2].name:
-				setSortOrder(2)
-				break
-
-			default:
-				break
-		}
-	}
-
-	const displayAssetList = (sortOptionIndex) => {
-		let sortedAssets = listOfAssets.sort(SORT_OPTIONS[sortOptionIndex].sortMethod)
-		let sortedAssetsList = sortedAssets.map((element, index) => {
-			return (
-				<AssetCard
-					key={index}
-					name={element.name}
-					type={element.type}
-					thumb={element.thumb}
-					created={element.created_at}
-					media={element}
-				/>
-			)
-		})
-		setSortAssets(sortedAssetsList)
-	}
 
 	const AssetCard = ({ name, thumb, created, type, media }) => {
 		return (
@@ -90,17 +53,63 @@ const MediaImporter = () => {
 		)
 	}
 
-	const SortOption = ({ sortType }) => {
+	const displayAssetList = () => {
+		const assets = sortedList.length >= 1 ? sortedList : listOfAssets
+		let sortedAssetsList
+		if (filterSearch != '') {
+			sortedAssetsList = assets
+				.filter((asset) => asset.name.toLowerCase().match(filterSearch))
+				.map((element, index) => {
+					console.log(element)
+					return (
+						<AssetCard
+							key={index}
+							name={element.name}
+							type={element.type}
+							thumb={element.thumb}
+							created={element.created_at}
+							media={element}
+						/>
+					)
+				})
+		} else {
+			sortedAssetsList = assets.map((element, index) => {
+				return (
+					<AssetCard
+						key={index}
+						name={element.name}
+						type={element.type}
+						thumb={element.thumb}
+						created={element.created_at}
+						media={element}
+					/>
+				)
+			})
+		}
+
+		setSortAssets(sortedAssetsList)
+	}
+
+	const SortOption = ({ sortTypeIndex }) => {
 		return (
 			<span
 				className="sort-option"
-				onClick={(ev) => {
-					sortAssetList(sortType)
+				onClick={() => {
+					setSortState({
+						...sortState,
+						sortAsc: !sortState.sortAsc,
+						sortOrder: sortTypeIndex,
+					})
 				}}
 			>
-				{sortType}
+				{SORT_OPTIONS[sortTypeIndex].name}
 			</span>
 		)
+	}
+
+	const filterFiles = (ev) => {
+		ev.preventDefault()
+		setFilterSearch(ev.target.value)
 	}
 
 	const RightPane = () => {
@@ -113,14 +122,22 @@ const MediaImporter = () => {
 
 				<div id="sort-bar">
 					<div className="sort-options">
-						<SortOption sortType={'Name'} />
-						<SortOption sortType={'Date'} />
-						<SortOption sortType={'Type'} />
+						<SortOption sortTypeIndex={0} />
+						<SortOption sortTypeIndex={1} />
+						<SortOption sortTypeIndex={2} />
+					</div>
+					<div>
+						<input
+							type="search"
+							placeholder="File Search"
+							onChange={filterFiles}
+							value={filterSearch}
+						/>
 					</div>
 				</div>
 
 				<div id="file-display">
-					{isSuccess === true && numOfAssets < 1 ? (
+					{isSuccess === true && numOfAssets <= 1 ? (
 						<div className="file-info">No files available!</div>
 					) : (
 						sortAssets
@@ -132,15 +149,24 @@ const MediaImporter = () => {
 
 	useEffect(() => {
 		if (mounted.current === true) {
-			if (numOfAssets > 0) {
-				displayAssetList(sortOrder)
-			}
+			displayAssetList()
 		}
-	}, [sortOrder, numOfAssets])
+	}, [filterSearch])
 
 	useEffect(() => {
 		if (mounted.current === true) {
+			const sortedAssets = listOfAssets.sort(SORT_OPTIONS[sortState.sortOrder].sortMethod)
+			const list = sortState.sortAsc ? sortedAssets : sortedAssets.reverse()
+			setSortedList(list)
+			displayAssetList()
+		}
+	}, [sortState])
+
+	// Jump start the displaying of assets list when loaded the first time.
+	useEffect(() => {
+		if (mounted.current === true) {
 			setNumOfAssets(listOfAssets.length)
+			displayAssetList()
 		}
 	}, [isSuccess])
 
