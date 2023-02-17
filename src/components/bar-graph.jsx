a1c87b3f0ec6c5d43cbb903320afe111ec836cc6
Corey Peterson <corey.peterson@ucf.edu>
2023-02-17 14:25:06 -0500

    Implements Emanuel's changes to score screen and distribution graphs

--- src/components/bar-graph.jsx
+++ src/components/bar-graph.jsx
@@ -1,77 +1,115 @@
-import React from 'react'
-import { Bar } from '@vx/shape';
-import { Group } from '@vx/group'
-import { scaleBand, scaleLinear } from '@vx/scale'
-import { AxisLeft, AxisBottom } from '@vx/axis'
-import { Grid } from '@vx/grid'
+import React, { useEffect, useRef } from 'react'
+import { axisBottom, axisLeft, scaleBand, scaleLinear, select } from 'd3'
 
-// accessors return the label and value of that data item
-const x = d => d.label
-// * 1.2 makes it so the max graph height will never be reached
-const y = d => 1.2 * d.value
+const BarGraph = ({ data, width, height, rowLabel = `Y Axis`, colLabel = `X Axis`, graphTitle = 'Title' }) => {
 
-const BarGraph = ({ data, width, height }) => {
-	// bounds
-	const xMax = width - 80
-	const yMax = height - 80
+	const linesColor = { color: `#a9a9a9` }
+	const margin = { top: 50, bottom: 25, left: 25, right: 25 }
+	const graphWidth = width - margin.left - margin.right
+	const graphHeight = height - margin.top - margin.bottom
 
-	const getScaleVal = () => data !== undefined ? Math.max(...data?.map(y)) : 0
+	// grade points / bars
+	const xAxis = scaleBand().domain(data.map(({ label }) => label)).range([0, graphWidth]).padding(0.5)
 
-	// scales
-	const xScale = scaleBand({
-		rangeRound: [0, xMax],
-		domain: data?.map(x),
-		padding: 0.4,
-	})
+	// num of students
+	const yAxis = scaleLinear().range([graphHeight, 0]).nice()
+	const largestNumStudents = Math.max(...data.map(({ value }) => value))
+	largestNumStudents === 1 ? yAxis.domain([0, 1.2]) : yAxis.domain([0, largestNumStudents])
 
-	const yScale = scaleLinear({
-		rangeRound: [0, yMax],
-		domain: [getScaleVal(), 0],
-	})
+	const ColAxis = ({ scale }) => {
+		const ref = useRef(null)
 
-	const barsRender = data?.map((d, i) => {
-		const label = d.label
-		const barWidth = xScale.bandwidth()
-		const barHeight = yMax - yScale(d.value)
-		const barX = xScale(label)
-		const barY = yMax - barHeight
-		return (
-			<Bar key={`bar-${label}`}
-				x={barX}
-				y={barY}
-				width={barWidth}
-				height={barHeight}>
-				<title>{d.value + ' scores'}</title>
-			</Bar>
-		)
-	})
+		useEffect(() => {
+			if (ref.current) { select(ref.current).call(axisLeft(scale)) }
+		}, [scale])
+
+		return <g ref={ref} style={linesColor} />
+	}
+
+	const RowAxis = ({ scale, transform }) => {
+		const ref = useRef(null)
+
+		useEffect(() => {
+			if (ref.current) { select(ref.current).call(axisBottom(scale)) }
+		}, [scale])
+
+		return <g ref={ref} transform={transform} style={linesColor} />
+	}
+
+	const VerticalLines = ({ scale, transform }) => {
+		const ref = useRef(null)
+
+		useEffect(() => {
+			if (ref.current) {
+				select(ref.current).call(axisBottom(scale)
+					.tickSize(-graphHeight, 0, 0)
+					.tickFormat("")
+				)
+			}
+		}, [scale])
+
+		return <g ref={ref} transform={transform} style={linesColor} />
+	}
+
+	const HorizontalLines = ({ scale }) => {
+		const ref = useRef(null)
+
+		useEffect(() => {
+			if (ref.current) {
+				select(ref.current).call(axisLeft(scale)
+					.tickSize(-graphWidth, 0, 0)
+					.tickFormat("")
+				)
+			}
+		}, [scale])
+
+		return <g ref={ref} style={linesColor} />
+	}
+
+	const Bars = ({ data, height, xAxis, yAxis }) => {
+		return (<>
+			{data.map(({ value, label }) => (
+				<rect
+					key={`bar-${label}`}
+					x={xAxis(label)}
+					y={yAxis(value)}
+					width={xAxis.bandwidth()}
+					height={height - yAxis(value)}
+					fill={`#0093e7`}
+				/>
+			))}
+		</>)
+	}
 
 	return (
-		<svg width={width}
-			height={height}>
-			<Group top={25}
-				left={40}>
-				<Grid left={13}
-					bottom={15}
-					xScale={xScale}
-					yScale={yScale}
-					width={xMax-10}
-					height={yMax}
-					numTicksRows={4}
-					numTicksColumns={11}
-				/>
-				<AxisLeft left={13}
-					scale={yScale}
-					numTicks={4}
-					label='Plays'
-				/>
-				{ barsRender }
-				<AxisBottom scale={xScale}
-					label='Score'
-					labelOffset={15}
-					top={yMax}
-				/>
-			</Group>
+		<svg width={width} height={height}>
+			<g transform={`translate(${margin.left + 20}, ${margin.top - 15})`}>
+				<HorizontalLines scale={yAxis} />
+				<VerticalLines scale={xAxis} transform={`translate(0, ${graphHeight})`} />
+
+				<ColAxis scale={yAxis} />
+				<RowAxis scale={xAxis} transform={`translate(0, ${graphHeight})`} />
+
+				<Bars data={data} height={graphHeight} xAxis={xAxis} yAxis={yAxis} />
+
+				<text
+					x={graphWidth * 0.5}
+					y={-10}
+					textAnchor={'middle'}
+				>
+					{graphTitle}
+				</text>
+				<text
+					x={graphHeight * -0.60}
+					y={-30}
+					style={{ transform: `rotate(-90deg)` }}
+				>
+					{colLabel}
+				</text>
+				<text transform={`translate(${graphWidth * 0.4}, ${graphHeight + margin.bottom * 1.5})`}>
+					{rowLabel}
+				</text>
+			</g>
 		</svg>
 	)
 }
--- src/components/my-widgets-score-semester-individual.jsx
+++ src/components/my-widgets-score-semester-individual.jsx
@@ -6,6 +6,7 @@ import MyWidgetScoreSemesterSummary from './my-widgets-score-semester-summary'
 import LoadingIcon from './loading-icon'
 
 const showScore = (instId, playId) => window.open(`/scores/single/${playId}/${instId}`)
+const _compareWidgets = (a, b) => { return (b.created_at - a.created_at) }
 
 const initState = () => ({
 	isLoading: true,
@@ -53,6 +54,8 @@ const MyWidgetScoreSemesterIndividual = ({ semester, instId }) => {
 					else {
 						for (let scoreIndex = 0; scoreIndex < scoreObject.scores.length; scoreIndex++)
 							newLogs[scoreObject.userId].scores.push(scoreObject.scores[scoreIndex])
+
+						newLogs[scoreObject.userId].scores.sort(_compareWidgets).reverse()
 					}
 				}
 
@@ -62,7 +65,6 @@ const MyWidgetScoreSemesterIndividual = ({ semester, instId }) => {
 	}, [isFetching])
 
 	useEffect(() => {
-
 		const logs = Object.values(logsList)
 		setState({ ...state, logs: logs, filteredLogs: logs, isLoading: false })
 
@@ -82,22 +84,22 @@ const MyWidgetScoreSemesterIndividual = ({ semester, instId }) => {
 
 		// unselect user if not in filtered results
 		const isSelectedInResults = filteredLogs.includes(state.selectedUser)
-		if (!isSelectedInResults) {
-			newState.selectedUser = {}
-		}
+		if (!isSelectedInResults) { newState.selectedUser = {} }
 		setState(newState)
-	},
-		[state.searchText, state.selectedUser, state.logs])
+
+	}, [state.searchText, state.selectedUser, state.logs])
 
 	const handleSearchChange = e => onSearchInput(e.target.value)
 
 	let mainContentRender = <LoadingIcon width='570px' />
 	if (!state.isLoading) {
 		const userRowElements = state.filteredLogs.map(user => (
-			<tr key={user.userId}
-				className={{ rowSelected: state.selectedUser.userId === user.userId }}
+			<tr
+				key={user.userId}
+				title={`View all scores for ${user.name}`}
 				onClick={() => { setState({ ...state, selectedUser: user }) }}
-				title={`View all scores for ${user.name}`}>
+				className={{ rowSelected: state.selectedUser.userId === user.userId }}
+			>
 				<td className={`listName ${state.selectedUser.userId === user.userId ? 'selected' : ''}`}>
 					{user.name}
 				</td>
@@ -107,9 +109,11 @@ const MyWidgetScoreSemesterIndividual = ({ semester, instId }) => {
 		let selectedUserRender = null
 		if (state.selectedUser.userId) {
 			const selectedUserScoreRows = state.selectedUser.scores.map(score => (
-				<tr key={score.playId}
+				<tr
+					key={score.playId}
+					title='View Detailed Scores for this Play'
 					onClick={() => { showScore(instId, score.playId) }}
-					title='View Detailed Scores for this Play'>
+				>
 					<td>{score.date}</td>
 					<td>{score.score}</td>
 					<td>{score.elapsed}</td>
