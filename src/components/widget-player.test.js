65a971a9df1fdf43837148c8cdc1d4cc892e8e56
Cay Henning <cay@ucf.edu>
2023-03-01 12:37:08 -0500

    Updates widget player unit tests

--- src/components/widget-player.test.js
+++ src/components/widget-player.test.js
@@ -1,8 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { act } from 'react-dom/test-utils';
-import { render, screen, cleanup, fireEvent, waitFor } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen, cleanup, fireEvent, waitFor, prettyDOM } from '@testing-library/react'
 
 import WidgetPlayer from './widget-player'
 import WidgetPlayerPage from './widget-player-page'
@@ -10,10 +13,12 @@ import WidgetPlayerPage from './widget-player-page'
 import * as api from '../util/api'
 
 import instance from '../__test__/mockapi/crossword_demo_instance.json'
-import widgetDetails from '../__test__/mockapi/widgets_get.json'
 import qset from '../__test__/mockapi/crossword_demo_qset.json'
         
 jest.mock('../util/api')
+
+// To see the DOM at any time after rendered has loaded, use:
+// console.log(prettyDOM(rendered.container))
 
 // Enables testing with react query
 const renderWithClient = async (children) => {
@@ -41,34 +46,58 @@ const renderWithClient = async (children) => {
 }
 
 describe('Widget Player Page', () => {
+	let mockApiGetWidgetInstance,
+	mockApiGetQuestionQset,
+	mockApiSavePlayLogs,
+	mockWindowAlert,
+	mockApiSaveStorage
 
-    it('should render', async () => {
-		let rendered;
+    // Setup the default mock implementations
+    beforeAll(() => {
+        mockApiGetWidgetInstance = jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(instance)
+		mockApiGetQuestionQset = jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(qset)
+		mockApiSavePlayLogs = jest.spyOn(api, 'apiSavePlayLogs').mockResolvedValue({status: 200, ok: true})
+		mockApiSaveStorage = jest.spyOn(api, 'apiSavePlayStorage').mockResolvedValue({})
+		mockWindowAlert = jest.spyOn(window, 'alert').mockImplementation(() => {});
+    })
 
+    // Restore default values before each test
+    // Setup window location pathname and hash value
+    beforeEach(() => {
+        mockApiGetWidgetInstance.mockResolvedValue(instance)
+		mockApiGetQuestionQset.mockResolvedValue(qset)
+		mockApiSavePlayLogs = jest.spyOn(api, 'apiSavePlayLogs').mockResolvedValue({status: 200, ok: true})
+		mockApiSaveStorage = jest.spyOn(api, 'apiSavePlayStorage').mockResolvedValue({})
+
+		window.PLAY_ID = 'XxSgi'
+		window.DEMO_ID = 'XxSgi'
+
+        Object.defineProperty(window, 'location', {
+            value: {
+				pathname: '/play/XxSgi/famous-landmarks',
+				assign: (url) => window.location = url
+			},
+            writable: true
+        });
+    })
+
+    // Because jest shares mocks between all test files
+    // We need to clear mock.calls, mock.results, and mock.instances
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+	it('should render widget iframe', async () => {
+        let rendered;
 		await act(async () => {
-			rendered = await renderWithClient(<WidgetPlayerPage />)
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
 		})
 
-		expect(rendered.container).not.toBeEmptyDOMElement()
-	})
-	
-})
-
-describe('Widget Player', () => {
-	afterEach(() => {
-		jest.clearAllMocks();
-	});
-
-	it('should render', async () => {
-		jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(instance)
-		jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(qset)
-
-		await act(async () => {
-			renderWithClient(<WidgetPlayer instanceId={instance.id} playId={""} minHeight={window.WIDGET_HEIGHT} minWidth={window.WIDGET_WIDTH} widgetURL={window.WIDGET_URL}/>)
-		})
-
-		expect(await screen.findByTitle('Widget Embed')).toHaveAttribute('src', expect.stringContaining(`${window.WIDGET_URL + instance.widget.dir + instance.widget.player}?${instance.widget.created_at}`));
-		expect(await screen.findByTitle('Widget Embed')).not.toBeNull()
+		// Does the iframe have the correct src attribute?
+		expect(await rendered.container.querySelector('iframe').getAttribute('src')).toBe(`${window.WIDGET_URL + instance.widget.dir + instance.widget.player}?${instance.widget.created_at}`);
+		
+		expect(mockApiGetWidgetInstance).toHaveBeenCalled();
+		expect(mockApiGetQuestionQset).toHaveBeenCalled();
 
 		// console.log(window._resourceLoader)
 		// To test iframe contents, must add to package.json:
@@ -79,37 +108,146 @@ describe('Widget Player', () => {
 	})
 
 	it('should render demo', async () => {
-		jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(instance)
-		jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(qset)
+		window.PLAY_ID = ""
 
+		let rendered;
 		await act(async () => {
-			renderWithClient(<WidgetPlayer instanceId={window.DEMO_ID} playId={""} minHeight={window.WIDGET_HEIGHT} minWidth={window.WIDGET_WIDTH} widgetURL={window.WIDGET_URL}/>)
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
 		})
 
-		expect(await screen.findByTitle('Widget Embed')).not.toBeNull()
+		expect(await rendered.container.querySelector('#container').getAttribute('src').includes(`${window.WIDGET_URL + instance.widget.dir + instance.widget.player}?${instance.widget.created_at}`)).toBeTruthy();
 	})
 
-	it('should fail if instance is undefined', async () => {
-		jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(undefined)
-		jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(qset)
-		jest.spyOn(window, 'alert').mockImplementation(() => {});
+	it('should send alert if instance is null', async () => {
+		jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(null)
 
+		let rendered;
 		await act(async () => {
-			await renderWithClient(<WidgetPlayer instanceId={instance.id} playId={""} minHeight={window.WIDGET_HEIGHT} minWidth={window.WIDGET_WIDTH} widgetURL={window.WIDGET_URL}/>)
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
 		})
 
-		expect(window.alert).toBeCalled()
+		expect(mockApiGetWidgetInstance).toHaveBeenCalled()
+		expect(mockWindowAlert).toHaveBeenCalled()
 	})
 
-	it('should fail if qset is undefined', async () => {
-		jest.spyOn(api, 'apiGetWidgetInstance').mockResolvedValue(instance)
-		jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(undefined)
-		jest.spyOn(window, 'alert').mockImplementation(() => {});
+	it('should send alert if qset is null', async () => {
+		jest.spyOn(api, 'apiGetQuestionSet').mockResolvedValue(null)
 
+		let rendered;
 		await act(async () => {
-			await renderWithClient(<WidgetPlayer instanceId={instance.id} playId={""} minHeight={window.WIDGET_HEIGHT} minWidth={window.WIDGET_WIDTH} widgetURL={window.WIDGET_URL}/>)
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
 		})
 
-		expect(window.alert).toBeCalled()
+		expect(mockApiGetQuestionQset).toHaveBeenCalled()
+		expect(mockWindowAlert).toHaveBeenCalled()
+
+	})
+
+	it('should not send alert if play log save succeeds', async () => {
+		let rendered;
+		await act(async () => {
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
+		})
+
+        const e_add = {
+            source: window,
+            origin: window.BASE_URL.substring(0, window.BASE_URL.length - 1),
+            data: {
+				type: 'addLog',
+				data: {
+					"value": 100,
+                    "text": "Everest",
+                    "id": "d6643f8c-31fd-46e8-b86c-76e5be6ac215"
+				}
+            }
+		}
+		
+        await act(async () => {
+            window.dispatchEvent(new MessageEvent('message', {...e_add, data: JSON.stringify(e_add.data)}))
+        })
+
+		// Push the log we added earlier
+		// sendPendingLogs calls _sendPendingPlayLogs which calls _pushPendingLogs which calls savePlayLog
+        const e_send = {
+            source: window,
+            origin: window.BASE_URL.substring(0, window.BASE_URL.length - 1),
+            data: {
+                type: 'sendPendingLogs',
+            }
+		}
+
+        await act(async () => {
+            window.dispatchEvent(new MessageEvent('message', {...e_send, data: JSON.stringify(e_send.data)}))
+        })
+		
+		expect(mockApiSavePlayLogs).toHaveBeenCalled()
+		expect(mockWindowAlert).not.toHaveBeenCalled()
+	})
+
+	it('should send alert if play log save fails', async () => {
+		jest.spyOn(api, 'apiSavePlayLogs').mockResolvedValue({halt: true, type: "error", msg: "You have been logged out, and must login again to continue"})
+
+		let rendered;
+		await act(async () => {
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
+		})
+
+        const e_add = {
+            source: window,
+            origin: window.BASE_URL.substring(0, window.BASE_URL.length - 1),
+            data: {
+				type: 'addLog',
+				data: {
+					"value": 100,
+                    "text": "Everest",
+                    "id": "d6643f8c-31fd-46e8-b86c-76e5be6ac215"
+				}
+            }
+		}
+		
+		// calls mockApiSavePlayLogs
+        await act(async () => {
+            window.dispatchEvent(new MessageEvent('message', {...e_add, data: JSON.stringify(e_add.data)}))
+        })
+
+		// Push the log we added earlier
+		// sendPendingLogs calls _sendPendingPlayLogs which calls _pushPendingLogs which calls savePlayLog
+        const e_send = {
+            source: window,
+            origin: window.BASE_URL.substring(0, window.BASE_URL.length - 1),
+            data: {
+                type: 'sendPendingLogs',
+            }
+		}
+
+        await act(async () => {
+            window.dispatchEvent(new MessageEvent('message', {...e_send, data: JSON.stringify(e_send.data)}))
+        })
+		
+		expect(mockApiSavePlayLogs).toHaveBeenCalled()
+		expect(mockWindowAlert).toHaveBeenCalled()
+	})
+
+	it('should show scoresceen on end', async () => {
+		let rendered;
+		await act(async () => {
+			rendered = await renderWithClient(<WidgetPlayerPage/>)
+		})
+		
+		const e_end = {
+            source: window,
+            origin: window.BASE_URL.substring(0, window.BASE_URL.length - 1),
+            data: {
+				type: 'end',
+				data: true // showScoreScreenAfter
+            }
+		}
+
+        await act(async () => {
+            window.dispatchEvent(new MessageEvent('message', {...e_end, data: JSON.stringify(e_end.data)}))
+		})
+		
+		expect(mockApiSavePlayLogs).toHaveBeenCalled()
+		expect(window.location).toEqual(`${window.BASE_URL}scores/${instance.id}#play-${window.PLAY_ID}`)
 	})
 })
