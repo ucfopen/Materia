36ed843181c27b90fa457799967c442d61062029
Cay Henning <cay@ucf.edu>
2023-03-07 14:10:16 -0500

    Renames migration to 54

--- fuel/app/migrations/054_add_support_user_role_perms.php
+++ fuel/app/migrations/054_add_support_user_role_perms.php
@@ -10,14 +10,14 @@ class Add_support_user_role_perms
 
         \DB::query('INSERT INTO `perm_role_to_perm` SET `role_id` = :role_id, `perm` = :perm ON DUPLICATE KEY UPDATE `perm` = :perm')
             ->param('role_id', $support_role_id)
-			->param('perm', \Materia\Perm::FULL)
+            ->param('perm', \Materia\Perm::FULL)
             ->execute();
 	}
 
 	public function down()
 	{
         $support_role_id = \Materia\Perm_Manager::get_role_id('support_user');
-        
+
         \DB::delete('perm_role_to_perm')
             ->where('role_id', 'like', $support_role_id)
             ->execute();
