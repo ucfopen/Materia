e390fbd0b0208ac7ab6dfad097757842e45aec15
Corey Peterson <corey.peterson@ucf.edu>
2023-05-04 14:23:57 -0400

    Obliteration of angularjs from codebase

--- src/student.js
+++
@@ -1,9 +0,0 @@
-require('./angular/controllers/ctrl-user-profile')
-require('./angular/controllers/ctrl-score-page')
-require('./angular/controllers/ctrl-user-settings')
-require('./angular/directives/dir-fullscreen')
-require('./materia/materia.scores.scoregraphics')
-require('./angular/services/srv-api')
-require('./angular/services/srv-scores')
-
-// Materia Server scripts used on all player or student pages
