fd67a2aaee750ff6e9f67bfbca4cedaa13e0ed65
Corey Peterson <corey.peterson@ucf.edu>
2023-07-28 16:39:54 -0400

    Polishes the accessibility indicator in the widget detail page

--- src/components/accessibility-indicator.jsx
+++ src/components/accessibility-indicator.jsx
@@ -15,12 +15,20 @@ const AccessibilityIndicator = ({widget = {}}) => {
 		}
 	}
 
+	let descriptionRender = ''
+	if (widget.accessibility.keyboard.toLowerCase() != 'unavailable' || widget.accessibility.screen_reader.toLowerCase() != 'unavailable') {
+		descriptionRender = widget.accessibility.description
+	}
+	else {
+		descriptionRender = 'No accessibility information is provided for this widget.'
+	}
+
 	return (
 		<div className='feature-list accessibility-options'>
 			<div className='list-holder'>
 				<span className='feature-heading'>Accessibility:</span>
 				<ul>
-					<li>
+					<li className={`accessibility-indicator ${widget.accessibility?.keyboard.toLowerCase() != 'unavailable' ? 'show' : ''}`}>
 						<div className='icon-spacer'>
 							<KeyboardIcon color='#5a5a5a'/>
 						</div>
@@ -30,15 +38,18 @@ const AccessibilityIndicator = ({widget = {}}) => {
 							</span>
 						</span>
 					</li>
-					<li>
+					<li className={`accessibility-indicator ${widget.accessibility?.screen_reader.toLowerCase() != 'unavailable' ? 'show' : ''}`}>
 						<div className='icon-spacer'>
 							<ScreenReaderIcon color='#5a5a5a'/>
 						</div>
-						<span>Screen reader is&nbsp;
+						<span>Screen readers are&nbsp;
 							<span id='screen-reader-access-level' className={`highlighted ${widget.accessibility?.screen_reader.toLowerCase()}`}>
 								{`${accessLevelToText(widget.accessibility?.screen_reader)} supported`}
 							</span>
 						</span>
+					</li>
+					<li className={`accessibility-description ${descriptionRender.length > 0 ? 'show' : ''}`}>
+						{descriptionRender}
 					</li>
 				</ul>
 			</div>
