eaddcdfd9105e43c613cf2c1d40a83207fd77ef2
Corey Peterson <corey.peterson@ucf.edu>
2024-01-09 17:05:06 -0500

    Modified property in API response unnecessary, pulling it back out

--- fuel/app/classes/materia/widget/instance/manager.php
+++ fuel/app/classes/materia/widget/instance/manager.php
@@ -89,8 +89,7 @@ class Widget_Instance_Manager
 		$displayable_inst = array_slice($displayable_inst, $offset, $widgets_per_page);
 
 		$data = [
-			'pagination' => $displayable_inst,
-			'modified'   => time(),
+			'pagination' => $displayable_inst
 		];
 		
 		if ($has_next_page) $data['next_page'] = $page_number + 1;
