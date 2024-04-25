48e688baeab56d1dea39022f9760485149b77639
Matthew Emond <me@ucf.edu>
2024-04-25 15:42:03 -0400

    Update user migration to handle empty username and duplicate username cases

--- app/core/migrations/0002_copy_users_to_django.py
+++ app/core/migrations/0002_copy_users_to_django.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from django.db import migrations
-from django.contrib.auth.models import User
+from django.db import migrations, IntegrityError, transaction
+
+from django.contrib.auth.models import User as DjangoUser
 
 # from core.models import Users
 
@@ -14,33 +15,57 @@ def copy_users_to_django(apps, schema_editor):
     # TODO: figure out how to handle profile_fields
     # TODO: look into bulk_create for potential efficiency
 
-    Users = apps.get_model("core", "Users")
+    FuelUsers = apps.get_model("core", "Users")
 
-    for user in Users.objects.all():
+    for fuel_user in FuelUsers.objects.all():
         # convert created_at and last_login to datetime
-        date_joined = datetime.fromtimestamp(user.created_at)
-        last_login = datetime.fromtimestamp(user.last_login)
+        date_joined = datetime.fromtimestamp(fuel_user.created_at)
+        last_login = datetime.fromtimestamp(fuel_user.last_login)
 
-        new_user = User.objects.create(
-            id=user.id,
-            password=user.password,
-            last_login=last_login,
-            is_superuser=False,
-            username=user.username,
-            first_name=user.first,
-            last_name=user.last,
-            email=user.email,
-            is_staff=False,
-            is_active=True,
-            date_joined=date_joined,
-        )
-        new_user.save()
+        if not fuel_user.username:
+            fuel_user.username = f"{fuel_user.id}{fuel_user.first}{fuel_user.last}"
+
+        try:
+            with transaction.atomic():
+                new_user = DjangoUser.objects.create(
+                    id=fuel_user.id,
+                    password=fuel_user.password,
+                    last_login=last_login,
+                    is_superuser=False,
+                    username=fuel_user.username,
+                    first_name=fuel_user.first,
+                    last_name=fuel_user.last,
+                    email=fuel_user.email,
+                    is_staff=False,
+                    is_active=True,
+                    date_joined=date_joined,
+                )
+                new_user.save()
+        except IntegrityError:
+            # duplicate username, make a new one
+            fuel_user.username = (
+                f"{fuel_user.username}_{fuel_user.id}{fuel_user.first}{fuel_user.last}"
+            )
+            new_user = DjangoUser.objects.create(
+                id=fuel_user.id,
+                password=fuel_user.password,
+                last_login=last_login,
+                is_superuser=False,
+                username=fuel_user.username,
+                first_name=fuel_user.first,
+                last_name=fuel_user.last,
+                email=fuel_user.email,
+                is_staff=False,
+                is_active=True,
+                date_joined=date_joined,
+            )
+            new_user.save()
 
 
 def revert_django_users_to_empty(apps, schema_editor):
     # delete all Django User objects
     try:
-        User.objects.all().delete()
+        DjangoUser.objects.all().delete()
     except Exception:
         pass
 
