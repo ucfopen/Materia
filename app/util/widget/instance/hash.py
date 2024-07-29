0af8cf6ec7824edb11210bfb9711ab7d397060ec
Brandon Stull <bstull@ucf.edu>
2024-07-29 11:39:09 -0400

    Replaced the key hash generator with something more sensical.

--- app/util/widget/instance/hash.py
+++ app/util/widget/instance/hash.py
@@ -1,16 +1,9 @@
-class WidgetInstanceHash():
-    # no idea how these were originally determined or if they're significant
-    GOLDEN_PRIMES = [
-        1,
-        41,
-        2377,
-        147299,
-        9132313,
-        566201239,
-        35104476161,
-        2176477521929
-    ]
+from math import floor
+from random import randint
+from sys import maxsize
+from uuid import uuid4
 
+class WidgetInstanceHash():
     # Unicode character codes for all relevant alphanumeric characters in base62 encoding
     #  0-9, a-z, and A-Z
     # see https://en.wikipedia.org/wiki/List_of_Unicode_characters
@@ -30,7 +23,6 @@ class WidgetInstanceHash():
     ]
 
     def base62(num):
-        from math import floor
         key = ''
         while num > 0:
             mod = num % 62
@@ -39,22 +31,13 @@ class WidgetInstanceHash():
         # return the reversed result of the above process
         return key[::-1]
 
-    #TODO: this is stupid, do it better
-    def generate_key_hash(length = 5):
-        from math import floor
-        from sys import maxsize
-        from random import randint
-        # this number is big enough, whatever
-        big_number = min(2147483647, maxsize)
-        num = randint(1, big_number)
-        ceil = pow(62, length)
-        prime = WidgetInstanceHash.GOLDEN_PRIMES[length]
-        dec = (num * prime) - floor(num * prime / ceil) * ceil
-        hash = WidgetInstanceHash.base62(dec)
+    def generate_key_hash(length=5):
+        ceil = min(maxsize, pow(62, length))
+        num = randint(1, ceil)
+        hash = WidgetInstanceHash.base62(num)
         # pad the hash with leading zeros up to the specified length
         return hash.zfill(length)
 
     # maybe just find all the places this is called and use uuid4 directly instead
     def generate_long_hash():
-        from uuid import uuid4
         return uuid4()
