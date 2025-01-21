5c1e8b2399785afa2b4833f0aa5b86ea33e14919
Christopher Solanilla <chrissolanilla@gmail.com>
2025-01-21 14:29:46 -0500

    rename docker folder

---
+++ docker/asset_backup.sql
@@ -0,0 +1,1 @@
+Enter password: mysqldump: Got error: 1045: "Access denied for user 'materia_user'@'localhost' (using password: YES)" when trying to connect
---
+++ docker/compose_local.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose.local.yml ${1:-up}
