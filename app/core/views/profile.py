a7ed7bf10b661cdf3bba8b239d7ad106919c7a22
Corey Peterson <corey.peterson@ucf.edu>
2025-03-05 11:19:54 -0500

    post- merge conflict fixes

--- app/core/views/profile.py
+++ app/core/views/profile.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.conf import settings as djangoSettings
 from django.shortcuts import render
 from .main import get_dark_mode
 
@@ -6,8 +6,8 @@ def profile(request):
     context = {
         "title": "Profile",
         "js_resources": ["dist/js/profile.js"],
-        "css_resources": settings.CSS_GROUPS["profile"],
-        "fonts": settings.FONTS_DEFAULT,
+        "css_resources": djangoSettings.CSS_GROUPS["profile"],
+        "fonts": djangoSettings.FONTS_DEFAULT,
         **get_dark_mode(request),
     }
     return render(request, "react.html", context)
@@ -17,8 +17,8 @@ def settings(request):
     context = {
         "title": "Settings",
         "js_resources": ["dist/js/settings.js"],
-        "css_resources": settings.CSS_GROUPS["settings"],
-        "fonts": settings.FONTS_DEFAULT,
+        "css_resources": djangoSettings.CSS_GROUPS["settings"],
+        "fonts": djangoSettings.FONTS_DEFAULT,
         **get_dark_mode(request),
     }
     return render(request, "react.html", context)
