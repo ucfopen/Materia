1619966f5e10232458fc23557b4561ddb4413a9f
fudgeu <patrickzambonie@gmail.com>
2025-05-08 12:40:38 -0400

    Fix small bug in exception handler

--- app/core/exception_handlers.py
+++ app/core/exception_handlers.py
@@ -9,10 +9,11 @@ def materia_exception_handler(exc, context):
 
     # Wrap the DRF error in a Msg object for easier front-end parsing
     if response is not None:
+        # TODO this can be expanded upon to catch more types of errors and handle them betterly
         if isinstance(exc, Http404):
             title = "Not Found"
         else:
-            title = exc.detail if hasattr(exc, "detail") else "Unknown Error"
+            title = exc.detail[0] if hasattr(exc, "detail") and isinstance(exc.detail, list) else "Unknown Error"
 
         msg = Msg(title=title, msg=response.data, halt=True, status=response.status_code)
         response = msg.as_json_response()
