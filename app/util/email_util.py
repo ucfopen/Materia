c9e2f8f5af17323182bbebe991d59c0def90dc92
fudgeu <patrickzambonie@gmail.com>
2025-10-15 11:28:02 -0400

    Add email command

--- app/util/email_util.py
+++ app/util/email_util.py
@@ -14,8 +14,8 @@ class EmailUtil:
         template: str,
         context: dict,
         plain_msg: str,
-        sender: User,
-        to: User,
+        sender: User | str,
+        to: User | str,
     ) -> bool:
         """
         Sends a Materia-themed email.
@@ -25,8 +25,8 @@ class EmailUtil:
             context: The context dict to fill the template.
             plain_msg: A plain string with the message's content, used when the reader does not view the HTML version of
             the email.
-            sender: User that is sending the email.
-            to: User that is receiving the email.
+            sender: User that is sending the email, or any string
+            to: User that is receiving the email, or any email string
 
         Returns:
             A boolean indicating whether the email was sent successfully.
@@ -39,13 +39,21 @@ class EmailUtil:
         # Append context with items required for root email template
         context["static_url"] = f"{settings.URLS["BASE_URL"]}static/"
 
+        # Determine from and to
+        to_email = to.email if isinstance(to, User) else to
+        from_str = (
+            f"{sender.first_name} {sender.last_name}"
+            if isinstance(sender, User)
+            else sender
+        )
+
         # Send email
         html = render_to_string(f"email/{template}", context)
         send_mail(
             subject=f"{settings.NAME} Notification",
             html_message=html,
             message=plain_msg,
-            from_email=f'"{sender.first_name} {sender.last_name}" <{settings.SYSTEM_EMAIL}>',
-            recipient_list=[to.email],
+            from_email=f'"{from_str}" <{settings.SYSTEM_EMAIL}>',
+            recipient_list=[to_email],
         )
         return True
