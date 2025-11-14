d03ac0a704a9b6658bf68e86800c8fc22632b12a
Brandon Stull <bstull@ucf.edu>
2025-11-14 09:50:54 -0500

    #126 Adding checks to login handling and url pattern definitions to optionally allow for overrides that enable external auth package usage. Moved some settings into their own files to more easily override them per environment/implementation.

---
+++ app/materia/settings/db.py
@@ -0,0 +1,15 @@
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+import os
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("MYSQL_DATABASE"),
+        "USER": os.environ.get("MYSQL_USER"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
+        "HOST": os.environ.get("MYSQL_HOST"),
+        "PORT": os.environ.get("MYSQL_PORT"),
+    },
+}
