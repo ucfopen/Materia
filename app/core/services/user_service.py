ffe0bc79ec6cdb4b74b2c9627ad005fa7d73ac44
GoldenLin9 <goldenlin0909@gmail.com>
2025-11-21 16:22:37 -0500

    updates user icon based on use gravatar boolean value

--- app/core/services/user_service.py
+++ app/core/services/user_service.py
@@ -1,5 +1,6 @@
 import hashlib
 
+from django.conf import settings
 from django.contrib.auth.models import User
 
 
@@ -7,6 +8,11 @@ class UserService:
 
     @staticmethod
     def get_avatar_url(user: User) -> str:
+        profile_settings = user.profile_settings
+        use_gravatar = profile_settings.get_profile_fields().get("useGravatar")
+        if not use_gravatar:
+            return f"{settings.STATIC_URL}img/default-avatar.jpg"
+
         clean_email = user.email.strip().lower().encode("utf-8")
         hash_email = hashlib.md5(clean_email).hexdigest()
         return f"https://www.gravatar.com/avatar/{hash_email}?d=retro&s=256"
