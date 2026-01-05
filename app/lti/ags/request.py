33e882558c440de74fe92d1e8e48c9cb0503f260
Corey Peterson <corey.peterson@ucf.edu>
2026-01-05 13:11:18 -0500

    Adds user-agent header to AGSRequest

--- app/lti/ags/request.py
+++ app/lti/ags/request.py
@@ -12,9 +12,13 @@ class AGSRequest:
         self.access_token = access_token
 
     def headers(self):
+        # Note: user-agent header is required by Canvas.
+        # we use PyLTI1p3-client since that's the user-agent provided by pylti1p3 for
+        # other LMS-directed requests under the hood
         headers = {
             "Authorization": f"Bearer {self.access_token}",
             "Content-Type": "application/json",
+            "User-Agent": "PyLTI1p3-client",
         }
 
         return headers
