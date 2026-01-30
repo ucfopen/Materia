3b05cb24bfc687c62b638366612b1b6c05189f9f
GoldenLin9 <goldenlin0909@gmail.com>
2026-01-30 14:41:40 -0500

    fixes typo

--- app/core/models.py
+++ app/core/models.py
@@ -1171,7 +1171,7 @@ class WidgetInstance(models.Model):
         return qsets
 
     def duplicate(
-        self, owner: User, new_name: str, copy_exiting_perms: bool = False
+        self, owner: User, new_name: str, copy_existing_perms: bool = False
     ) -> Self:
         dupe = WidgetInstance.objects.get(pk=self.pk)
 
@@ -1211,7 +1211,7 @@ class WidgetInstance(models.Model):
         dupe_qset.save()
 
         # Copy perms, if requested
-        if copy_exiting_perms:
+        if copy_existing_perms:
             existing_perms = self.permissions.all()
             for existing_perm in existing_perms:
                 dupe.permissions.create(
