cd35fd2fd9eee56a213b4d4b076015af1268bedf
Corey Peterson <corey.peterson@ucf.edu>
2026-05-29 15:02:12 -0400

    Reorders migration 0029

--- app/core/migrations/0029_siteimage.py
+++ app/core/migrations/0029_siteimage.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0027_ltiplaystate_score_submitted"),
+        ("core", "0028_remove_logplay_log_play_is_complete_and_more"),
     ]
 
     operations = [
