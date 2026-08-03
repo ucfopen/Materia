6c3a90209b2b478ae9349bc45edbfcd7c1e13417
Corey Peterson <corey.peterson@ucf.edu>
2026-08-03 15:23:09 -0400

    Community library resources updated to use new import and react query v5 syntax

--- src/components/community-library-detail-page.jsx
+++ src/components/community-library-detail-page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useQuery } from '@tanstack/react-query'
 import { apiGetLibraryEntry, apiGetWidget } from '../util/api'
 import Header from './header'
 import CommunityLibrary from './community-library'
--- src/components/community-library-page.jsx
+++ src/components/community-library-page.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useQuery } from '@tanstack/react-query'
 import { apiGetWidget } from '../util/api'
 import Header from './header'
 import CommunityLibrary from './community-library'
 
 const CommunityLibraryPage = () => {
 	const { data: widgets } = useQuery({
-		queryKey: 'catalog-widgets',
+		queryKey: ['catalog-widgets'],
 		queryFn: () => apiGetWidget([], 'catalog'),
 		staleTime: Infinity,
 	})
